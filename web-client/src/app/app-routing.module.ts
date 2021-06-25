fced44d0bb2772d1d58de860f46298a0aea2d240
Jonathan Ngbonga <jongbonga@gmail.com>
2021-06-25 17:36:02 +0200

    create views folder

--- web-client/src/app/app-routing.module.ts
+++ web-client/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./views/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
+  }
 ];
 
 @NgModule({
@@ -19,4 +20,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
