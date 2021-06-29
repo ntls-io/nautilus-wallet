c633233f0ff44ab5f472acbf831ff2bd102c7622
Pi Delport <pi@registree.io>
2021-06-29 12:06:01 +0200

    test(web-client): add missing await

--- web-client/e2e/src/app.e2e-spec.ts
+++ web-client/e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should be blank', () => {
-    page.navigateTo();
+  it('should be blank', async () => {
+    await page.navigateTo();
     expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
   });
 });
