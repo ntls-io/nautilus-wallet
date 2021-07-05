f8eb3f956f08dec0c6ce1af1c0d32d1a70c63ffb
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-06 00:20:37 +0200

    add QRCode display to wallet page

--- web-client/src/app/views/wallet/wallet.module.ts
+++ web-client/src/app/views/wallet/wallet.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { QRCodeModule } from 'angularx-qrcode';
 import { WalletPageRoutingModule } from './wallet-routing.module';
 import { WalletPage } from './wallet.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, WalletPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    WalletPageRoutingModule,
+    QRCodeModule,
+  ],
   declarations: [WalletPage],
 })
 export class WalletPageModule {}
