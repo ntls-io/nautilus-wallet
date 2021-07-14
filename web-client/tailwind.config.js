b5f55213ac91cdbc200353719162d4f951467a1c
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-14 23:55:11 +0200

    refactor(ui): run prettier

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{html,ts}'],
+  mode: "jit",
+  purge: ["./src/**/*.{html,ts}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'prussian-blue': 'var(--ion-color-prussian-blue)',
-        'usafa-blue': 'var(--ion-color-usafa-blue)',
-        'viridian-green': 'var(--ion-color-viridian-green)',
-        'portland-orange': 'var(--ion-color-portland-orange)',
-        'blue-jeans': 'var(--ion-color-blue-jeans)',
-        gainsboro: 'var(--ion-color-gainsboro)',
-        'caribbean-green': 'var(--ion-color-caribbean-green)',
-        mustard: 'var(--ion-color-mustard)',
-        'jungle-green': 'var(--ion-color-jungle-green)',
-        'indigo-dye': 'var(--ion-color-indigo-dye)',
-        'morning-blue': 'var(--ion-color-morning-blue)'
-      }
-    }
+        "prussian-blue": "var(--ion-color-prussian-blue)",
+        "usafa-blue": "var(--ion-color-usafa-blue)",
+        "viridian-green": "var(--ion-color-viridian-green)",
+        "portland-orange": "var(--ion-color-portland-orange)",
+        "blue-jeans": "var(--ion-color-blue-jeans)",
+        gainsboro: "var(--ion-color-gainsboro)",
+        "caribbean-green": "var(--ion-color-caribbean-green)",
+        mustard: "var(--ion-color-mustard)",
+        "jungle-green": "var(--ion-color-jungle-green)",
+        "indigo-dye": "var(--ion-color-indigo-dye)",
+        "morning-blue": "var(--ion-color-morning-blue)",
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
   plugins: [
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/typography')
-  ]
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/typography"),
+  ],
 };
