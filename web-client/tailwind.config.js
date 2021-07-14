c241d13e2fa9131dc53d239c0ffaf85a8b51c9ce
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-14 22:12:14 +0200

    feat(ui): extend tailwind colors

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -1,17 +1,31 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./src/**/*.{html,ts}"],
+  mode: 'jit',
+  purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'prussian-blue': '#003450',
+        'usafa-blue': '#025581',
+        'viridian-green': '#209C95',
+        'portland-orange': '#FF642E',
+        'blue-jeans': '#02A7FF',
+        gainsboro: '#DADFDF',
+        'caribbean-green': '#11CA92',
+        mustard: '#FFD952',
+        'jungle-green': '#1A4C48',
+        'indigo-dye': '#00507A',
+        'morning-blue': '#92A2A1'
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/typography"),
-  ],
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
+  ]
 };
