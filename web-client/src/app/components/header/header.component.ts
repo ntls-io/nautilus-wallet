fc11bccb049fe33d57b590988cd9a16e24239789
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-18 22:29:49 +0200

    build(web-client): run prettier

--- web-client/src/app/components/header/header.component.ts
+++ web-client/src/app/components/header/header.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
