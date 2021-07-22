684fddf60e8b3b03b4366f4c8a5b6b16d209503b
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-22 16:00:36 +0200

    feat(web-client): add wallet page elements

--- web-client/src/app/views/wallet/wallet.page.ts
+++ web-client/src/app/views/wallet/wallet.page.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallet.page.scss'],
 })
 export class WalletPage implements OnInit {
+  actionItems = [
+    {
+      title: 'Send Money',
+      icon: 'cash',
+    },
+    {
+      title: 'Top Up Wallet',
+      icon: 'cash',
+    },
+    {
+      title: 'Withdraw',
+      icon: 'archive',
+    },
+    {
+      title: 'Receive',
+      icon: 'qr-code',
+    },
+    {
+      title: 'My Transactions',
+      icon: 'receipt',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
