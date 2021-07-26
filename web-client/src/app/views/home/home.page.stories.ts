7c136f1bedfd5679c4b4d7ce7313e28bfbae27f2
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-26 16:17:18 +0200

    feat(web-client): declare components in story

--- web-client/src/app/views/home/home.page.stories.ts
+++ web-client/src/app/views/home/home.page.stories.ts
@@ -1,9 +1,21 @@
-import { Meta, Story } from '@storybook/angular';
+import { Meta, moduleMetadata, Story } from '@storybook/angular';
+import { ActionItemComponent } from 'src/app/components/action-item/action-item.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { ProfileCardHorizontalComponent } from 'src/app/components/profile-card-horizontal/profile-card-horizontal.component';
 import { HomePage } from './home.page';
 
 export default {
   title: 'Views/HomePage',
   component: HomePage,
+  decorators: [
+    moduleMetadata({
+      declarations: [
+        HeaderComponent,
+        ActionItemComponent,
+        ProfileCardHorizontalComponent,
+      ],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<HomePage> = (args: HomePage) => ({
