0606696dea48af445fe76ed36fdfa2ccb1a448eb
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-26 16:23:22 +0200

    feat(web-client): add RouterTestingModule to spec

--- web-client/src/app/components/action-item/action-item.component.spec.ts
+++ web-client/src/app/components/action-item/action-item.component.spec.ts
@@ -1,22 +1,24 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ActionItemComponent } from './action-item.component';
 
 describe('ActionItemComponent', () => {
   let component: ActionItemComponent;
   let fixture: ComponentFixture<ActionItemComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ActionItemComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ActionItemComponent],
+        imports: [IonicModule.forRoot(), RouterTestingModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ActionItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ActionItemComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
