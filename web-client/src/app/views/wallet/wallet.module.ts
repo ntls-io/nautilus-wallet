1adc55e864f9be5e2a9a3c0ee6b58d89e9500404
Jonathan Ngbonga <jongbonga@gmail.com>
2021-07-28 11:30:47 +0200

    feat(web-client): add header

--- web-client/src/app/views/wallet/wallet.module.ts
+++ web-client/src/app/views/wallet/wallet.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 import { WalletPageRoutingModule } from './wallet-routing.module';
 import { WalletPage } from './wallet.page';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, WalletPageRoutingModule],
-  declarations: [WalletPage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [WalletPage, HeaderComponent],
 })
 export class WalletPageModule {}
