29036aef7c9128df00d80a4872305dd2bbebc926
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-03 15:42:49 +0200

    feat(web-client): update test helper
    accomodate ion-item as navigation trigger

--- web-client/src/tests/test.helpers.ts
+++ web-client/src/tests/test.helpers.ts
@@ -10,25 +10,26 @@ import { Router } from '@angular/router';
 /**
  * Verify that the fixture has a button that successfully navigates to the given `routerLink`.
  */
-export const verifyButtonNavigation = async (
+export const verifyNavigationTrigger = async (
   router: Router,
   fixture: ComponentFixture<any>,
   routerLink: string
 ): Promise<void> => {
   expect(router.url).toBe('/');
-  const button = getButtonWithLink(fixture.debugElement, routerLink);
-  button.nativeElement.click();
+  const trigger = getElementWithLink(fixture.debugElement, routerLink);
+  trigger.nativeElement.click();
   await fixture.whenStable();
   expect(router.url).toBe(routerLink);
 };
 
-const getButtonWithLink = (
+const getElementWithLink = (
   element: DebugElement,
   routerLink: string
 ): DebugElement => {
-  const button = element.query(
-    By.css(`ion-button[routerLink="${routerLink}"]`)
-  );
-  expect(button).not.toBeNull();
-  return button;
+  const trigger =
+    element.query(By.css(`ion-button[routerLink="${routerLink}"]`)) ??
+    element.query(By.css(`ion-item[ng-reflect-router-link="${routerLink}"]`));
+
+  expect(trigger).not.toBeNull();
+  return trigger;
 };
