6789e114b015199913b476853132124d3a63aa76
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-04 20:31:39 +0200

    refactor(web-client): run prettier

--- web-client/src/app/views/receive/receive-routing.module.ts
+++ web-client/src/app/views/receive/receive-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { ReceivePage } from './receive.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ReceivePage
-  }
+    component: ReceivePage,
+  },
 ];
 
 @NgModule({
