7a3bcfa2545dba9b8747f0db0a5c48366a6e4f23
Jonathan Ngbonga <jonathan@registree.io>
2021-08-16 12:29:03 +0200

    build(web-client): install & setup akita (#46)
    * build(web-client): install & setup akita
    
    * refactor(web-client): move akita cli to devDep

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -2,8 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco/transloco-root.module';
@@ -18,8 +22,16 @@ import { TranslocoRootModule } from './transloco/transloco-root.module';
     HttpClientModule,
     TranslocoRootModule,
     FontAwesomeModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AkitaNgRouterStoreModule,
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: NG_ENTITY_SERVICE_CONFIG,
+      useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
