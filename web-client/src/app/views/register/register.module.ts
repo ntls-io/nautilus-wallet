ff6e2ff61d2f10545d9bff7b52468b53c6023788
Tsundzukani <trnghondzweni@gmail.com>
2021-08-19 23:51:13 +0200

    feat(web-client): added form validators and fixed styling on register page

--- web-client/src/app/views/register/register.module.ts
+++ web-client/src/app/views/register/register.module.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Input, NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { RegisterPageRoutingModule } from './register-routing.module';
 import { RegisterPage } from './register.page';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, RegisterPageRoutingModule, SharedModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    IonicModule,
+    RegisterPageRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
   declarations: [RegisterPage],
 })
 export class RegisterPageModule {}
--- web-client/src/app/views/register/register.page.ts
+++ web-client/src/app/views/register/register.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,25 +7,62 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  registrationForm: FormGroup;
+  public registrationForm: FormGroup;
 
+  
   constructor(private formBuilder: FormBuilder) {
     this.initializeForm();
   }
-
+  
   ngOnInit() {}
-
-  initializeForm(): void{
+  
+  initializeForm(): void {
     this.registrationForm = this.formBuilder.group({
-      firstName: '',
-      lastName: '',
-      mobile: '',
-      password: '',
-      confirmPassword: ''
-    })
-  };
-
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      mobile: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(10),
+        ]),
+      ],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(10),
+        ]),
+      ],
+      confirmPassword: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(10),
+        ]),
+      ],
+    }, {validator: this.passwordValidator});
+  }
+  
   onSubmit(): void {
-    console.log(this.registrationForm.value);
+    console.log(this.registrationForm);
+    console.log(this.registrationForm.controls);
+
+  }
+
+  get f() {
+    return this.registrationForm.controls;
+  }
+
+  passwordValidator(control: AbstractControl): { [key: string]:boolean } | null {
+    const password =control.get('password');
+    const confirmPassword= control.get('confirmPassword');
+
+    return password && confirmPassword && password.value !== confirmPassword.value ?
+    {'misMatch' : true} :
+    null;
   }
 }
