2211eaf43018a804079da204644ee12f52dc249d
Pi Delport <pi@registree.io>
2021-08-20 13:20:17 +0200

    fixup! feat(web-client): schema for wallet communication

--- web-client/src/app/views/register/register.page.ts
+++ web-client/src/app/views/register/register.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CreateWallet } from '../../../schema/actions';
-import { WalletService } from '../../wallet.service';
 
 @Component({
   selector: 'app-register',
@@ -8,18 +6,7 @@ import { WalletService } from '../../wallet.service';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  constructor(private walletService: WalletService) {}
+  constructor() {}
 
-  ngOnInit() {
-    const request: CreateWallet = {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      auth_pin: '1234',
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      owner_name: 'Test Owner',
-    };
-
-    this.walletService
-      .createWallet(request)
-      .subscribe((created) => console.log('XXX created!', created));
-  }
+  ngOnInit() {}
 }
