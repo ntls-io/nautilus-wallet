5eb47a427baca09c5d411d4b8fb72c9f4a84c05f
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-20 11:40:53 +0200

    feat(web-client): add ability to print address

--- web-client/src/app/views/print-wallet/print-wallet.module.ts
+++ web-client/src/app/views/print-wallet/print-wallet.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { NgxPrinterModule } from 'ngx-printer';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { PrintWalletPageRoutingModule } from './print-wallet-routing.module';
 import { PrintWalletPage } from './print-wallet.page';
@@ -13,6 +14,7 @@ import { PrintWalletPage } from './print-wallet.page';
     IonicModule,
     PrintWalletPageRoutingModule,
     SharedModule,
+    NgxPrinterModule,
   ],
   declarations: [PrintWalletPage],
 })
