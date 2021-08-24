2ec6dace32a81428811000cd2d4f45d09782874e
Tsundzukani <trnghondzweni@gmail.com>
2021-08-24 23:50:23 +0200

    feat(web-client): Added form validation for non-valid submission

--- web-client/src/app/views/register/register.page.ts
+++ web-client/src/app/views/register/register.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,61 +13,67 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 })
 export class RegisterPage implements OnInit {
   public registrationForm: FormGroup;
+  nonValidSubmit: boolean = true;
 
-  
   constructor(private formBuilder: FormBuilder) {
     this.initializeForm();
   }
-  
+
   ngOnInit() {}
-  
+
   initializeForm(): void {
-    this.registrationForm = this.formBuilder.group({
-      firstName: ['', Validators.compose([Validators.required])],
-      lastName: ['', Validators.compose([Validators.required])],
-      mobile: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(10),
-          Validators.maxLength(10),
-        ]),
-      ],
-      password: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(10),
-        ]),
-      ],
-      confirmPassword: [
-        '',
-        Validators.compose([
-          Validators.required,
-          Validators.minLength(4),
-          Validators.maxLength(10),
-        ]),
-      ],
-    }, {validator: this.passwordValidator});
+    this.registrationForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.compose([Validators.required])],
+        lastName: ['', Validators.compose([Validators.required])],
+        mobile: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(10),
+            Validators.maxLength(10),
+          ]),
+        ],
+        pin: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(10),
+          ]),
+        ],
+        confirmPin: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(10),
+          ]),
+        ],
+      },
+      { validator: this.pinValidator }
+    );
   }
-  
+
   onSubmit(): void {
     console.log(this.registrationForm);
     console.log(this.registrationForm.controls);
-
   }
 
   get f() {
     return this.registrationForm.controls;
   }
 
-  passwordValidator(control: AbstractControl): { [key: string]:boolean } | null {
-    const password =control.get('password');
-    const confirmPassword= control.get('confirmPassword');
+  pinValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const pin = control.get('pin');
+    const confirmPin = control.get('confirmPin');
 
-    return password && confirmPassword && password.value !== confirmPassword.value ?
-    {'misMatch' : true} :
-    null;
+    return pin && confirmPin && pin.value !== confirmPin.value
+      ? { misMatch: true }
+      : null;
+  }
+
+  showErrors() {
+    this.nonValidSubmit = false;
   }
 }
