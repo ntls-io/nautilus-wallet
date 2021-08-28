9c192af2f3fca22070f8510955128de27a75075d
Herman <herman@registree.io>
2021-08-28 17:07:35 +0200

    test(web-client): add wallet access page tests

--- web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
+++ web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { ScannerPage } from '../scanner/scanner.page';
+import { ScannerService } from 'src/app/services/scanner.service';
+import Swal from 'sweetalert2';
 import { WalletAccessPage } from './wallet-access.page';
 
 describe('WalletAccessPage', () => {
   let component: WalletAccessPage;
   let fixture: ComponentFixture<WalletAccessPage>;
+  let scannerService: ScannerService;
+  let modalCtrl: ModalController;
 
   beforeEach(
     waitForAsync(() => {
@@ -15,11 +20,42 @@ describe('WalletAccessPage', () => {
 
       fixture = TestBed.createComponent(WalletAccessPage);
       component = fixture.componentInstance;
+      scannerService = TestBed.get(ScannerService);
+      modalCtrl = TestBed.get(ModalController);
       fixture.detectChanges();
     })
   );
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#openScanner should fire alert if camera permission request fails', async () => {
+    // TODO: Implement this by wrapping swal in a service or using ngx-sweetalert2
+
+    spyOn(scannerService, 'requestPermissions').and.resolveTo(false);
+    const swalSpy = spyOn(Swal, 'fire');
+    await component.openScanner();
+    expect(swalSpy).toHaveBeenCalled();
+  });
+
+  it('#openScanner should create the a modal with the scanner page if camera permission request succeeds', async () => {
+    const modalSpy = jasmine.createSpyObj('Modal', {
+      present: Promise.resolve(),
+      onWillDismiss: Promise.resolve('result'),
+    });
+
+    const modalCreateSpy = spyOn(modalCtrl, 'create')
+      .withArgs({
+        component: ScannerPage,
+      })
+      .and.resolveTo(modalSpy);
+
+    spyOn(scannerService, 'requestPermissions').and.resolveTo(true);
+
+    await component.openScanner();
+
+    expect(modalCreateSpy).toHaveBeenCalled();
+    expect(modalSpy.present).toHaveBeenCalled();
   });
 });
