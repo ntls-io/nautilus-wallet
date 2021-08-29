37342706cbe29ffa89c302d6cde219820bb9785f
Jonathan Ngbonga <jonathan@registree.io>
2021-08-29 19:51:39 +0200

    feat(web-client): add lock screen (#55)
    * feat(web-client): add lock screen
    
    * test(web-client): add storybook
    
    * refactor(web-client): remove keyboard UI
    
    * feat(web-client): bind keyboard keys
    
    * build(web-client): add Keyboard plugin
    
    * feat(web-client): show keyboard if native device
    
    * refactor(web-client): remove default passcode
    
    * test(web-client): add storybook default args
    
    * refactor(web-client): update lock with input field
    
    * test(web-client): remove default args from stories
    
    * refactor(web-client): add mask plugin & validation
    
    * test(web-client): add reactive forms module
    
    * refactor(web-client): share ReactiveFormsModule
    
    * test(web-client): import shared modules
    
    * fix(web-client): fix app routing path
    
    * fix(web-client): revert package lock changes
    
    Co-authored-by: Herman <herman@registree.io>

--- web-client/src/app/modules/shared/shared.module.ts
+++ web-client/src/app/modules/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IonicModule } from '@ionic/angular';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { QRCodeModule } from 'angularx-qrcode';
+import { BrMaskerModule } from 'br-mask';
 import { ActionItemComponent } from 'src/app/components/action-item/action-item.component';
 import { HeaderComponent } from 'src/app/components/header/header.component';
 import { ProfileCardHorizontalComponent } from 'src/app/components/profile-card-horizontal/profile-card-horizontal.component';
@@ -15,13 +17,15 @@ import { ProfileCardHorizontalComponent } from 'src/app/components/profile-card-
     ActionItemComponent,
     ProfileCardHorizontalComponent,
   ],
-  imports: [IonicModule, CommonModule, RouterModule],
+  imports: [IonicModule, CommonModule, RouterModule, BrMaskerModule],
   exports: [
     FontAwesomeModule,
     QRCodeModule,
     HeaderComponent,
     ProfileCardHorizontalComponent,
     ActionItemComponent,
+    BrMaskerModule,
+    ReactiveFormsModule,
     ZXingScannerModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
