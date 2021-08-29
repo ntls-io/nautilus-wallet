33ff452ad994bae53cff7245cf05b7162b6d7dc4
Tsundzukani <trnghondzweni@gmail.com>
2021-08-29 22:01:14 +0200

    Add registration page (#54)
    * feat(web-client): add registration page
    
    Co-authored-by: Jonathan Ngbonga <jonathan@registree.io>
    Co-authored-by: Herman <herman@registree.io>
    Co-authored-by: Jonathan Ngbonga <jongbonga@gmail.com>

--- web-client/src/app/views/register/register.module.ts
+++ web-client/src/app/views/register/register.module.ts
@@ -2,11 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { RegisterPageRoutingModule } from './register-routing.module';
 import { RegisterPage } from './register.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, RegisterPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RegisterPageRoutingModule,
+    SharedModule,
+  ],
   declarations: [RegisterPage],
 })
 export class RegisterPageModule {}
