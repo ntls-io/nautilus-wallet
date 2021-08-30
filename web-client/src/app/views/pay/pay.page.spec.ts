120fa0303d4fa47f3849e29fce5c0ecec0aa90a3
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-30 09:52:25 +0200

    test(web-client): add router module to spec

--- web-client/src/app/views/pay/pay.page.spec.ts
+++ web-client/src/app/views/pay/pay.page.spec.ts
@@ -1,22 +1,25 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { PayPage } from './pay.page';
 
 describe('PayPage', () => {
   let component: PayPage;
   let fixture: ComponentFixture<PayPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PayPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PayPage],
+        imports: [IonicModule.forRoot(), RouterTestingModule, SharedModule],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(PayPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(PayPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
