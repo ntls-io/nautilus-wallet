74b461ac145a2bc7409caae235e748fbb74acf76
Jonathan Ngbonga <jongbonga@gmail.com>
2021-08-30 20:27:50 +0200

    feat(web-client): add success & error alert

--- web-client/src/app/views/pay/pay.page.ts
+++ web-client/src/app/views/pay/pay.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pay',
@@ -11,7 +13,11 @@ export class PayPage implements OnInit {
   wallet!: string;
   paymentForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private navCtrl: NavController
+  ) {
     this.paymentForm = this.formBuilder.group({
       amount: [0, Validators.compose([Validators.required])],
     });
@@ -27,7 +33,72 @@ export class PayPage implements OnInit {
     this.paymentForm.markAllAsTouched();
 
     if (this.paymentForm.valid) {
-      //()=>{send payment}
+      //TODO: ()=>{send payment}
+      //TODO: Display notification
+      // if (success) {
+      //   this.notifySuccess(
+      //     'R250',
+      //     'G6AIRDAJFSBXNFBHLQ2F5JLZJ6EYYYLDZSCDHUQUB2YUG5QO4ZB4VNAL7I',
+      //     'LNJH2Q2BYMV7KMMFMEEUCKIW6C3HPBOL4ARZBPPE2OBVVDPNVN6A',
+      //     new Date()
+      //   );
+      // } else {
+      //   this.notifyError(errorMessage);
+      // }
     }
+  }
+
+  notifySuccess(
+    amount: string,
+    address: string,
+    txid: string,
+    timestamp: Date
+  ) {
+    Swal.fire({
+      icon: 'success',
+      titleText: 'Money sent!',
+      text: `Your money was sent successfully.`,
+      html: `<div >
+              <h2 class="text-primary font-bold">${amount}</h2>
+              <p class="text-xs">${address}</p>
+              <small>Completed on ${timestamp.toLocaleString()}</small>
+            </div>`,
+      confirmButtonColor: 'var(--ion-color-primary)',
+      confirmButtonText: 'DONE',
+      customClass: {
+        confirmButton: 'w-1/2 !rounded-full',
+      },
+      backdrop: true,
+      heightAuto: false,
+      allowOutsideClick: false,
+      footer: `<a href="https://algoexplorer.io/tx/${txid}" target="_blank" >TxID</a>`,
+    }).then(({ isConfirmed }) => {
+      if (isConfirmed) {
+        this.navCtrl.navigateRoot('wallet');
+      }
+    });
+  }
+
+  notifyError(text: string) {
+    Swal.fire({
+      icon: 'error',
+      titleText: 'Oops!',
+      text,
+      confirmButtonColor: 'var(--ion-color-primary)',
+      confirmButtonText: 'RETRY',
+      customClass: {
+        confirmButton: 'w-1/2 !rounded-full',
+      },
+      showCloseButton: true,
+      backdrop: true,
+      heightAuto: false,
+      allowOutsideClick: false,
+    }).then(({ isConfirmed, isDismissed }) => {
+      if (isConfirmed) {
+        //TODO: retry payment
+      } else if (isDismissed) {
+        this.navCtrl.navigateRoot('wallet');
+      }
+    });
   }
 }
