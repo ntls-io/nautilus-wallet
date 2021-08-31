eefbbfe3973aeee55a3c07d312d95a595857f37d
Herman <herman@registree.io>
2021-08-31 08:30:50 +0200

    feat(web-client): link up sign-up flow

--- web-client/src/app/views/landing/landing.page.ts
+++ web-client/src/app/views/landing/landing.page.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { resetStores } from '@datorama/akita';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage implements OnInit {
-  constructor() {}
+  constructor() {
+    resetStores();
+  }
 
   ngOnInit() {}
 }
--- web-client/src/app/views/print-wallet/print-wallet.page.ts
+++ web-client/src/app/views/print-wallet/print-wallet.page.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Clipboard } from '@capacitor/clipboard';
 import { ToastController } from '@ionic/angular';
+import { WalletQuery } from 'src/app/wallet.query';
 
 @Component({
   selector: 'app-print-wallet',
@@ -10,7 +11,12 @@ import { ToastController } from '@ionic/angular';
 export class PrintWalletPage implements OnInit {
   @Input() wallet!: string;
 
-  constructor(private toastCtrl: ToastController) {}
+  constructor(
+    private toastCtrl: ToastController,
+    private walletQuery: WalletQuery
+  ) {
+    this.wallet = walletQuery.getValue().walletId;
+  }
 
   ngOnInit() {}
 
