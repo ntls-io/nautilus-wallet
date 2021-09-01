d5f4829fca7b8bfd048cf4dea6df98a3822b931a
Pi Delport <pi@registree.io>
2021-09-01 19:45:09 +0200

    feat(web-client): add API schema and service (#59)
    * deps(web-client): add @msgpack/msgpack, algosdk, tweetnacl
    
    * deps(web-client): add @types/superagent
    
      npm install --save-dev @types/superagent
    
    * chore(web-client): add workaround for Wepback 5 dropping crypto polyfill
    
    * style(web-client): update @typescript-eslint/naming-convention rule
    
      This is based on the defaultConfig here:
    
      https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming- 
      convention.md#options
    
      with the following additions for our codebase:
    
      * default, variable: snake_case
      * property: snake_case, PascalCase
    
    * feat(web-client): schema for wallet communication
    
    * feat(web-client): add WalletService
    
    * feat(web-client): add transaction confirmation helpers
    
    * test(web-client, WalletService): test array helpers
    
    * test(web-client): add test helper: withResolveContext
    
    * test(web-client): add test helper: withNestedResolveContexts
    
    * test(web-client, WalletService): test wallet API methods
    
    * test(web-client, algorand.helpers): test makePaymentTxnHelper

---
+++ web-client/src/schema/crypto.ts
@@ -0,0 +1,43 @@
+/**
+ * Interface patterned after `SodaBoxCrypto` on the server, but implemented using TweetNaCl.
+ */
+
+import * as nacl from 'tweetnacl';
+import { BoxKeyPair } from 'tweetnacl';
+import { Bytes, Bytes24, Bytes32 } from './types';
+
+export type PublicKey = Bytes32;
+export type PrivateKey = Bytes32;
+export type Nonce = Bytes24;
+
+type EncryptedMessage = {
+  ciphertext: Bytes;
+  nonce: Bytes24;
+};
+
+export class TweetNaClCrypto {
+  constructor(public keyPair: BoxKeyPair) {}
+
+  static new = (): TweetNaClCrypto => new TweetNaClCrypto(nacl.box.keyPair());
+
+  get public_key(): PublicKey {
+    return this.keyPair.publicKey;
+  }
+
+  get secret_key(): PrivateKey {
+    return this.keyPair.secretKey;
+  }
+
+  decrypt_message = (
+    ciphertext: Bytes,
+    their_pk: PublicKey,
+    nonce: Nonce
+  ): Uint8Array | null =>
+    nacl.box.open(ciphertext, nonce, their_pk, this.secret_key);
+
+  encrypt_message = (message: Bytes, their_pk: PublicKey): EncryptedMessage => {
+    const nonce = nacl.randomBytes(nacl.box.nonceLength);
+    const ciphertext = nacl.box(message, nonce, their_pk, this.secret_key);
+    return { ciphertext, nonce };
+  };
+}
--- web-client/src/tests/test.helpers.ts
+++ web-client/src/tests/test.helpers.ts
@@ -3,7 +3,11 @@
  */
 
 import { DebugElement } from '@angular/core';
-import { ComponentFixture } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flushMicrotasks,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
@@ -34,3 +38,56 @@ const getElementWithLink = (
   expect(trigger).not.toBeNull();
   return trigger;
 };
+
+/**
+ * Resolve a promise with dynamic context provided by a callable.
+ *
+ * This is intended to make it slightly less awkward to use Promise-based code with Angular's HTTP testing pattern,
+ * which requires request expectations to be made strictly _after_ the code under test makes its requests,
+ * but _before_ awaiting completion of the code under test.
+ *
+ * With this helper, `const result = await codeUnderTest()` becomes:
+ *
+ * ```
+ * const result = await withResolveContext(codeUnderTest(), () => {
+ *   // Use httpTestingController.expectOne(…)
+ * })
+ * ```
+ *
+ * @see https://angular.io/guide/http#testing-http-requests
+ *
+ * @param promise Promise to await
+ * @param context callable to invoke before awaiting `promise`
+ */
+export const withResolveContext = async <T>(
+  promise: Promise<T>,
+  context: () => void
+): Promise<T> => {
+  context();
+  return await promise;
+};
+
+/**
+ * Like {@link withResolveContext}, but support a series of nested dynamic contexts.
+ *
+ * This can be used when a promised depends on more thane
+ *
+ * The implementation of this function more tricky and subtle:
+ * it relies on careful use of Angular's {@link fakeAsync} and {@link flushMicrotasks}
+ * to manually advance the resolving task's execution before calling each context.
+ *
+ * @param promise Promise to return
+ * @param contexts A series of context callables to be called in order,
+ *                 interleaved with advancing the execution of `promise`
+ */
+export const withNestedResolveContexts = <R>(
+  promise: Promise<R>,
+  contexts: Array<() => void>
+): Promise<R> =>
+  fakeAsync(() => {
+    for (const context of contexts) {
+      flushMicrotasks();
+      context();
+    }
+    return promise;
+  })();
