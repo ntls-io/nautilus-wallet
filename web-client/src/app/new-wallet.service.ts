806992b77004667ecf09fd78a085989fe2a087f0
Pi Delport <pi@registree.io>
2021-09-02 15:40:51 +0200

    feat(web-client): add never type-assertion helper

--- web-client/src/app/new-wallet.service.ts
+++ web-client/src/app/new-wallet.service.ts
@@ -78,3 +78,13 @@ export class NewWalletService {
     }
   }
 }
+
+/**
+ * Helper for exhaustiveness checking: mark unreachable values.
+ *
+ * TODO(Pi): Move this into a utility module somewhere.
+ */
+export const never = (value: never): never => {
+  console.error('expected never, got:', value);
+  throw new TypeError('expected never, got value (see error log)');
+};
