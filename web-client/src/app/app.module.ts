7083951b415084a48da9a3e0e7ebb5c95c690c14
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-02 16:25:18 +0200

    feat(web-client): add sentry

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
@@ -7,11 +7,23 @@ import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import * as SentryAngular from '@sentry/angular';
+import * as Sentry from '@sentry/capacitor';
+import { Integrations } from '@sentry/tracing';
 import { NgxPrinterModule } from 'ngx-printer';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco/transloco-root.module';
+
+Sentry.init(
+  {
+    //TODO: set dsn after account created
+    // dsn: '',
+    integrations: [new Integrations.BrowserTracing()],
+  },
+  SentryAngular.init
+);
 
 @NgModule({
   declarations: [AppComponent],
@@ -32,6 +44,11 @@ import { TranslocoRootModule } from './transloco/transloco-root.module';
     {
       provide: NG_ENTITY_SERVICE_CONFIG,
       useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' },
+    },
+    {
+      provide: ErrorHandler,
+      // Attach the Sentry ErrorHandler
+      useValue: SentryAngular.createErrorHandler(),
     },
   ],
   bootstrap: [AppComponent],
