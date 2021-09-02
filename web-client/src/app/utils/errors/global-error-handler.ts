b6f4377475455b1bebfe68144e6d80a3b515cb9a
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-02 22:34:43 +0200

    feat(web-client): return to landing after error

--- web-client/src/app/utils/errors/global-error-handler.ts
+++ web-client/src/app/utils/errors/global-error-handler.ts
@@ -1,16 +1,26 @@
 import { ErrorHandler, Injectable, NgZone } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { SwalHelper } from '../notification/swal-helper';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
-  constructor(private zone: NgZone, private notification: SwalHelper) {}
+  constructor(
+    private zone: NgZone,
+    private notification: SwalHelper,
+    private navCtrl: NavController
+  ) {}
 
   handleError(error: Error) {
-    this.zone.run(() =>
-      this.notification.swal.fire({
-        icon: 'error',
-        text: error.message || 'Undefined client error',
-      })
+    this.zone.run(
+      async () =>
+        await this.notification.swal
+          .fire({
+            icon: 'error',
+            text: error.message || 'Undefined client error',
+          })
+          .then(() => {
+            this.navCtrl.navigateRoot('');
+          })
     );
   }
 }
