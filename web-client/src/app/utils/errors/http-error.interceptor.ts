55a02a215c7800eb3de5bc91612488fc89f00480
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-02 21:25:19 +0200

    feat(web-client): add http interceptor

---
+++ web-client/src/app/utils/errors/http-error.interceptor.ts
@@ -0,0 +1,34 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
+import { SwalHelper } from '../notification/swal-helper';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor(private notification: SwalHelper) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('Error from error interceptor', error);
+
+        this.notification.swal.fire({
+          icon: 'error',
+          text: error.message ?? JSON.stringify(error),
+        });
+        return throwError(error);
+      }),
+      finalize(() => {})
+    ) as Observable<HttpEvent<any>>;
+  }
+}
