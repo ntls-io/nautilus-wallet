a03be116615f6c5f407bd1b9bd802414aa9c101d
Pi Delport <pi@registree.io>
2021-09-03 21:01:16 +0200

    refactor(ScannerPage): rework event handling and result returning

--- web-client/src/app/views/scanner/scanner.page.ts
+++ web-client/src/app/views/scanner/scanner.page.ts
@@ -1,27 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
+/** A scan modal result */
+export type ScanResult =
+  | { type: 'scanSuccess'; result: string }
+  | { type: 'scanError'; error: Error }
+  | { type: 'camerasNotFound'; error?: DOMException }
+  | { type: 'permissionDenied' }
+  | { type: 'dismissed' };
+
+/**
+ * QR code scanning dialog.
+ *
+ * Returns the scan result as a {@link ScanResult} via ModalController dismiss.
+ */
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.page.html',
   styleUrls: ['./scanner.page.scss'],
 })
 export class ScannerPage implements OnInit {
+  @ViewChild(ZXingScannerComponent, { static: false })
+  scanner!: ZXingScannerComponent;
+
   constructor(private modalCtrl: ModalController) {}
 
   ngOnInit() {}
 
-  scanSuccessHandler(data: any) {
-    this.modalCtrl.dismiss(data);
+  /** Dismiss the modal, returning the given result. */
+  async returnScanResult(scanResult: ScanResult) {
+    console.log('ScannerPage returning scan result:', scanResult);
+    await this.modalCtrl.dismiss(scanResult);
+  }
+
+  async dismissModal() {
+    await this.returnScanResult({ type: 'dismissed' });
+  }
+
+  // Modal event handlers:
+
+  async scanSuccessHandler(result: string) {
+    await this.returnScanResult({ type: 'scanSuccess', result });
   }
 
   //TODO: handle scan error
-  /* istanbul ignore next TODO */
-  scanErrorHandler(event: any) {
-    console.log(event);
+  async scanErrorHandler(error: Error) {
+    await this.returnScanResult({ type: 'scanError', error });
   }
 
-  dismissModal() {
-    this.modalCtrl.dismiss();
+  // TODO: UI to select device?
+  camerasFoundHandler(devices: MediaDeviceInfo[]) {
+    console.log('ScannerPage camerasFound:', devices);
+  }
+
+  async camerasNotFoundHandler(error?: DOMException) {
+    await this.returnScanResult({ type: 'camerasNotFound' });
+  }
+  async permissionResponseHandler(hasPermission: boolean) {
+    console.log('ScannerPage hasPermission:', hasPermission);
+    if (!hasPermission) {
+      await this.returnScanResult({ type: 'permissionDenied' });
+    }
   }
 }
