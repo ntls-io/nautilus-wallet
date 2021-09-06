574c6c1027d69c8680d90be92f3857c532c51e5f
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-06 13:55:23 +0200

    test(web-client): run prettier

--- web-client/src/app/views/manual-address/manual-address-routing.module.ts
+++ web-client/src/app/views/manual-address/manual-address-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { ManualAddressPage } from './manual-address.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ManualAddressPage
-  }
+    component: ManualAddressPage,
+  },
 ];
 
 @NgModule({
