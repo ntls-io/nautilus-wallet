f219428b5ad1ec9b70d08a058e831e4e5f4eda94
Herman <herman@registree.io>
2021-09-09 14:12:02 +0200

    feat(web-server): add wallet operation to sign accept asset transactions

--- web-server/sgx-wallet-impl/src/wallet_operations/sign_transaction.rs
+++ web-server/sgx-wallet-impl/src/wallet_operations/sign_transaction.rs
@@ -1,102 +1,173 @@
-use std::prelude::v1::ToString;
-
-use crate::schema::actions::{SignTransaction, SignTransactionResult};
-use crate::schema::entities::AlgorandTransactionSigned;
-use crate::wallet_operations::errors;
-use crate::wallet_operations::store::load_wallet;
+use crate::schema::actions::{SignAcceptAssetTransaction, SignTransaction, SignTransactionResult};
 
 type Result = SignTransactionResult;
 
+pub fn sign_accept_asset_transaction(
+    request: &SignAcceptAssetTransaction,
+) -> SignTransactionResult {
+    match sign_impl::sign_accept_asset_transaction(request) {
+        Ok(ok) => Result::Signed(ok),
+        Err(err) => err,
+    }
+}
+
 pub fn sign_transaction(request: &SignTransaction) -> SignTransactionResult {
-    // Load identified wallet
-    let result = match load_wallet(&request.wallet_id) {
-        Ok(ok) => ok,
-        Err(err) => return Result::Failed(err.to_string()),
-    };
-    let stored = match result {
-        None => return Result::InvalidAuth,
-        Some(some) => some,
-    };
-
-    // Verify request's authenticating PIN
-    if request.auth_pin != stored.auth_pin {
-        return Result::InvalidAuth;
+    match sign_impl::sign_transaction(request) {
+        Ok(ok) => Result::Signed(ok),
+        Err(err) => err,
     }
+}
 
-    // Authenticated! Proceed to sign the transaction.
+mod sign_impl {
+    use std::prelude::v1::{Box, Result, ToString};
 
-    // Safety: `split_at` panics if len < mid.
-    if request.algorand_transaction_bytes.len() < 2 {
-        return Result::Failed(errors::message_with_base64(
-            "sign_transaction",
-            "transaction too short",
-            format!("len = {}", request.algorand_transaction_bytes.len()),
-            "unsigned transaction msgpack",
-            &request.algorand_transaction_bytes,
-        ));
-    }
+    use algonaut::transaction::account::Account;
+    use algonaut::transaction::{Transaction, TransactionType};
 
-    // Check and strip the "TX" prefix tag
-    let transaction_bytes_without_prefix = match request.algorand_transaction_bytes.split_at(2) {
-        (b"TX", rest) => rest,
-        (unrecognised, _rest) => {
-            return Result::Failed(errors::message_with_base64(
-                "sign_transaction",
-                "transaction prefix tag not recognised",
-                format!("expected TX, got {:?}", unrecognised),
-                "unsigned transaction msgpack",
-                &request.algorand_transaction_bytes,
-            ))
+    use super::algorand_network_compatible;
+    use crate::schema::actions::{
+        SignAcceptAssetTransaction,
+        SignTransaction,
+        SignTransactionResult,
+    };
+    use crate::schema::entities::{AlgorandTransactionSigned, WalletStorable};
+    use crate::schema::types::WalletId;
+    use crate::wallet_operations::errors;
+    use crate::wallet_operations::store::load_wallet;
+
+    pub(super) fn sign_accept_asset_transaction(
+        request: &SignAcceptAssetTransaction,
+    ) -> Result<AlgorandTransactionSigned, SignTransactionResult> {
+        let stored = get_wallet(&request.wallet_id)?;
+
+        // TODO: Authenticated! Proceed to sign the transaction.
+        let transaction_to_sign = deserialize_transaction(&request.algorand_transaction_bytes)?;
+
+        // Ensure that we only proceed if the transaction type is correct, and the fee is 0
+        if (matches!(
+            transaction_to_sign.txn_type,
+            TransactionType::AssetAcceptTransaction(..)
+        ) && transaction_to_sign.fee.0 == 0)
+        {
+            let signing_account = &stored.algorand_account.as_algonaut_account();
+            let signed_transaction_bytes =
+                sign_transaction_struct(signing_account, &transaction_to_sign)?;
+            Ok(AlgorandTransactionSigned {
+                signed_transaction_bytes,
+            })
+        } else {
+            // TODO: Is InvalidAuth correct here
+            Err(SignTransactionResult::InvalidAuth)
         }
-    };
+    }
 
-    let transaction_to_sign =
-        &match algorand_network_compatible::from_msgpack(transaction_bytes_without_prefix) {
+    pub(super) fn sign_transaction(
+        request: &SignTransaction,
+    ) -> Result<AlgorandTransactionSigned, SignTransactionResult> {
+        let stored = get_wallet(&request.wallet_id)?;
+
+        // Verify request's authenticating PIN
+        if request.auth_pin != stored.auth_pin {
+            return Err(SignTransactionResult::InvalidAuth);
+        }
+
+        // Authenticated! Proceed to sign the transaction.
+
+        let transaction_to_sign = deserialize_transaction(&request.algorand_transaction_bytes)?;
+
+        let signing_account = &stored.algorand_account.as_algonaut_account();
+        let signed_transaction_bytes =
+            sign_transaction_struct(signing_account, &transaction_to_sign)?;
+        Ok(AlgorandTransactionSigned {
+            signed_transaction_bytes,
+        })
+    }
+
+    /// Loads the wallet corresponding to the provided wallet ID using the wallet store.
+    fn get_wallet(wallet_id: &WalletId) -> Result<WalletStorable, SignTransactionResult> {
+        let result = match load_wallet(wallet_id) {
+            Ok(ok) => ok,
+            Err(err) => return Err(SignTransactionResult::Failed(err.to_string())),
+        };
+        match result {
+            None => Err(SignTransactionResult::InvalidAuth),
+            Some(some) => Ok(some),
+        }
+    }
+
+    fn deserialize_transaction(
+        algorand_transaction_bytes: &Box<[u8]>,
+    ) -> Result<Transaction, SignTransactionResult> {
+        // Safety: `split_at` panics if len < mid.
+        if algorand_transaction_bytes.len() < 2 {
+            return Err(SignTransactionResult::Failed(errors::message_with_base64(
+                "sign_transaction",
+                "transaction too short",
+                format!("len = {}", algorand_transaction_bytes.len()),
+                "unsigned transaction msgpack",
+                &algorand_transaction_bytes,
+            )));
+        }
+
+        // Check and strip the "TX" prefix tag
+        let transaction_bytes_without_prefix = match algorand_transaction_bytes.split_at(2) {
+            (b"TX", rest) => rest,
+            (unrecognised, _rest) => {
+                return Err(SignTransactionResult::Failed(errors::message_with_base64(
+                    "sign_transaction",
+                    "transaction prefix tag not recognised",
+                    format!("expected TX, got {:?}", unrecognised),
+                    "unsigned transaction msgpack",
+                    &algorand_transaction_bytes,
+                )))
+            }
+        };
+
+        // Note: Intentionally serialize with algonaut's `to_msg_pack` helper, not ours.
+        match algorand_network_compatible::from_msgpack(transaction_bytes_without_prefix) {
+            Ok(ok) => Ok(ok),
+            Err(err) => Err(SignTransactionResult::Failed(errors::message_with_base64(
+                "sign_transaction",
+                "failed to unpack received transaction-to-be-signed",
+                err,
+                "unsigned transaction msgpack",
+                &algorand_transaction_bytes,
+            ))),
+        }
+    }
+
+    fn sign_transaction_struct(
+        signing_account: &Account,
+        transaction_to_sign: &Transaction,
+    ) -> Result<Box<[u8]>, SignTransactionResult> {
+        let signed_transaction = &match signing_account.sign_transaction(transaction_to_sign) {
             Ok(ok) => ok,
             Err(err) => {
-                return Result::Failed(errors::message_with_base64(
-                    "sign_transaction",
-                    "failed to unpack received transaction-to-be-signed",
-                    err,
-                    "unsigned transaction msgpack",
-                    &request.algorand_transaction_bytes,
+                return Err(SignTransactionResult::Failed(
+                    errors::message_with_debug_value(
+                        "sign_transaction",
+                        "algonaut error while signing transaction",
+                        err,
+                        "unsigned transaction",
+                        transaction_to_sign,
+                    ),
                 ))
             }
         };
 
-    let signing_account = &stored.algorand_account.as_algonaut_account();
-
-    let signed_transaction = &match signing_account.sign_transaction(transaction_to_sign) {
-        Ok(ok) => ok,
-        Err(err) => {
-            return Result::Failed(errors::message_with_debug_value(
-                "sign_transaction",
-                "algonaut error while signing transaction",
-                err,
-                "unsigned transaction",
-                transaction_to_sign,
-            ))
+        match algorand_network_compatible::to_msgpack(signed_transaction) {
+            Ok(ok) => Ok(ok),
+            Err(err) => Err(SignTransactionResult::Failed(
+                errors::message_with_debug_value(
+                    "sign_transaction",
+                    "failed to pack signed ",
+                    err,
+                    "signed transaction msgpack",
+                    signed_transaction,
+                ),
+            )),
         }
-    };
-
-    // Note: Intentionally serialize with algonaut's `to_msg_pack` helper, not ours.
-    let signed_transaction_bytes = match algorand_network_compatible::to_msgpack(signed_transaction)
-    {
-        Ok(ok) => ok,
-        Err(err) => {
-            return Result::Failed(errors::message_with_debug_value(
-                "sign_transaction",
-                "failed to pack signed ",
-                err,
-                "signed transaction msgpack",
-                signed_transaction,
-            ))
-        }
-    };
-
-    Result::Signed(AlgorandTransactionSigned {
-        signed_transaction_bytes,
-    })
+    }
 }
 
 /// Algorand network-compatible MessagePack serialization and deserialization.
