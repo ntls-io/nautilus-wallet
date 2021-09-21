e4599181f1615a4de9ce18f138a42f995a0cd545
Pi Delport <pi@registree.io>
2021-09-21 15:54:22 +0200

    refactor(asset-services-worker): simplify run_helpers import

--- asset-services/asset-services-worker/src/main.rs
+++ asset-services/asset-services-worker/src/main.rs
@@ -1,11 +1,11 @@
 use std::error::Error;
 
-use asset_services_celery::run_helpers::{init_celery_from_env, init_logging_from_env};
+use asset_services_celery::run_helpers;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    init_logging_from_env();
-    let celery = init_celery_from_env().await?;
+    run_helpers::init_logging_from_env();
+    let celery = run_helpers::init_celery_from_env().await?;
     celery.display_pretty().await;
 
     celery.consume().await?;
