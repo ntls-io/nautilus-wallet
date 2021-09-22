09cfc3a0074252cb273c259a314e3e1bbbdf56c5
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-22 14:31:55 +0200

    refactor(web-client): update & apply input mask

--- web-client/src/app/views/pay/pay.module.ts
+++ web-client/src/app/views/pay/pay.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { InputMaskModule } from '@ngneat/input-mask';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { PayPageRoutingModule } from './pay-routing.module';
 import { PayPage } from './pay.page';
@@ -14,6 +15,7 @@ import { PayPage } from './pay.page';
     IonicModule,
     PayPageRoutingModule,
     SharedModule,
+    InputMaskModule.forRoot({ inputSelector: 'input', isAsync: true }),
   ],
   declarations: [PayPage],
 })
