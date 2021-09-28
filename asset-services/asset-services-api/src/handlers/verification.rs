6359aaf5200587220759fe18a9650d937c45f32a
Pi Delport <pi@registree.io>
2021-09-28 14:57:56 +0200

    feat(asset-services-api): expand verification handlers

--- asset-services/asset-services-api/src/handlers/verification.rs
+++ asset-services/asset-services-api/src/handlers/verification.rs
@@ -1,11 +1,17 @@
 use std::sync::Arc;
+use std::time::Duration;
 
 use asset_services_celery::celery_box::CeleryBox;
+use asset_services_celery::exports::Signature;
+use asset_services_celery::task_records::redis_impl::RedisTaskRecords;
 use asset_services_celery::tasks;
-use axum::extract;
+use asset_services_celery::tasks::verification::{VerificationCheckResponse, VerificationResponse};
+use axum::{extract, Json};
 use serde::Deserialize;
 
 use crate::errors::AnyhowErrorResponse;
+
+const DEFAULT_TIMEOUT: Duration = Duration::from_secs(5);
 
 #[derive(Deserialize)]
 pub(crate) struct StartVerify {
@@ -18,17 +24,33 @@ pub(crate) struct CheckVerify {
     code: String,
 }
 
+// Convert requests to task signatures:
+
+impl From<StartVerify> for Signature<tasks::verification::start_verify> {
+    fn from(StartVerify { phone_number }: StartVerify) -> Self {
+        tasks::verification::start_verify::new(phone_number)
+    }
+}
+
+impl From<CheckVerify> for Signature<tasks::verification::check_verify> {
+    fn from(CheckVerify { sid, code }: CheckVerify) -> Self {
+        tasks::verification::check_verify::new(sid, code)
+    }
+}
+
 /// Send a verification code to a phone number.
 ///
 /// See: [`tasks::verification::start_verify`]
 pub(crate) async fn start_verify(
     extract::Extension(celery): extract::Extension<Arc<CeleryBox>>,
-    extract::Json(StartVerify { phone_number }): extract::Json<StartVerify>,
-) -> Result<String, AnyhowErrorResponse> {
-    let sent = celery
-        .send_task(tasks::verification::start_verify::new(phone_number))
+    Json(start_verify): Json<StartVerify>,
+) -> Result<Json<VerificationResponse>, AnyhowErrorResponse> {
+    // XXX: Connection per request.
+    let verification_response = RedisTaskRecords::from_env()
+        .await?
+        .execute_task(celery.as_ref(), start_verify.into(), DEFAULT_TIMEOUT)
         .await?;
-    Ok(sent.task_id)
+    Ok(Json(verification_response))
 }
 
 /// Check a received verification code.
@@ -36,10 +58,12 @@ pub(crate) async fn start_verify(
 /// Call [`tasks::verification::check_verify`]
 pub(crate) async fn check_verify(
     extract::Extension(celery): extract::Extension<Arc<CeleryBox>>,
-    extract::Json(CheckVerify { sid, code }): extract::Json<CheckVerify>,
-) -> Result<String, AnyhowErrorResponse> {
-    let sent = celery
-        .send_task(tasks::verification::check_verify::new(sid, code))
+    extract::Json(check_verify): extract::Json<CheckVerify>,
+) -> Result<Json<VerificationCheckResponse>, AnyhowErrorResponse> {
+    // XXX: Connection per request.
+    let verification_check_response = RedisTaskRecords::from_env()
+        .await?
+        .execute_task(celery.as_ref(), check_verify.into(), DEFAULT_TIMEOUT)
         .await?;
-    Ok(sent.task_id)
+    Ok(Json(verification_check_response))
 }
