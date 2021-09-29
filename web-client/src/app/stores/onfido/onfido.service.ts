baa65d27b1c53278e9c0030e7307c20f0391a243
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-29 14:29:49 +0200

    feat(web-client): add getToken mehtod

--- web-client/src/app/stores/onfido/onfido.service.ts
+++ web-client/src/app/stores/onfido/onfido.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { WalletQuery } from 'src/app/wallet.query';
 import { OnfidoStore } from './onfido.store';
 
 @Injectable({ providedIn: 'root' })
 export class OnfidoService {
+  constructor(
+    private onfidoStore: OnfidoStore,
+    private http: HttpClient,
+    private walletQuery: WalletQuery
+  ) {}
 
-  constructor(private onfidoStore: OnfidoStore, private http: HttpClient) {
+  getToken() {
+    const { name } = this.walletQuery.getValue();
+    return this.http
+      .post('kyc/start', name)
+      .pipe(tap((token) => this.onfidoStore.update(token)));
   }
-
-  get() {
-    return this.http.get('').pipe(tap(entities => this.onfidoStore.update(entities)));
-  }
-
 }
