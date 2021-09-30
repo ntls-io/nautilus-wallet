9f1fe5952056a05a5651db5a66195b5f095dc888
Jonathan Ngbonga <jongbonga@gmail.com>
2021-09-30 16:13:14 +0200

    refactor(web-client): remove unused get call

--- web-client/src/app/stores/session/session.service.ts
+++ web-client/src/app/stores/session/session.service.ts
@@ -1,16 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
-import { SessionStore } from './session.store';
 
 @Injectable({ providedIn: 'root' })
 export class SessionService {
-
-  constructor(private sessionStore: SessionStore, private http: HttpClient) {
-  }
-
-  get() {
-    return this.http.get('').pipe(tap(entities => this.sessionStore.update(entities)));
-  }
-
+  constructor() {}
 }
