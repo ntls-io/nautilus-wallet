25383b4843896d4daf0b8cafd325e6051e85960f
Tsundzukani <trnghondzweni@gmail.com>
2021-10-04 22:40:10 +0200

    feat:(web-client): ripple_keypairs implementation

--- web-server/sgx-wallet-impl/src/wallet_operations/create_wallet.rs
+++ web-server/sgx-wallet-impl/src/wallet_operations/create_wallet.rs
@@ -1,18 +1,20 @@
 use std::prelude::v1::ToString;
 
 use crate::schema::actions::{CreateWallet, CreateWalletResult};
-use crate::schema::entities::{AlgorandAccount, WalletDisplay, WalletStorable};
+use crate::schema::entities::{AlgorandAccount, XrpAccount, WalletDisplay, WalletStorable};
 use crate::wallet_operations::store::save_new_wallet;
 
 type Result = CreateWalletResult;
 
 pub fn create_wallet(request: &CreateWallet) -> Result {
     let new_account = AlgorandAccount::generate();
+    let new_xrp_account = XrpAccount::generate();
     let storable = WalletStorable {
         wallet_id: new_account.address_base32(),
         owner_name: request.owner_name.clone(),
         auth_pin: request.auth_pin.clone(),
         algorand_account: new_account,
+        xrp_account: new_xrp_account,
     };
     match save_new_wallet(&storable) {
         Ok(()) => Result::Created(WalletDisplay::from(storable)),
