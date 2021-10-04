8b5a6541d168465898ea04c063baa549896f40ac
Jonathan Ngbonga <jongbonga@gmail.com>
2021-10-04 21:31:46 +0200

    feat(web-client): add send funds function

--- web-client/src/app/stores/xrp-faucet/xrp-faucet.service.ts
+++ web-client/src/app/stores/xrp-faucet/xrp-faucet.service.ts
@@ -1,7 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RippleAPI } from 'ripple-lib';
 import { tap } from 'rxjs/operators';
 import { XrpFaucetStore } from './xrp-faucet.store';
+
+const ripple = new RippleAPI({
+  server: '', //TODO: set server address
+});
 
 @Injectable({ providedIn: 'root' })
 export class XrpFaucetService {
@@ -19,5 +24,35 @@ export class XrpFaucetService {
       .toPromise();
 
     this.xrpFaucetStore.setLoading(false);
+  }
+
+  async sendFunds(
+    fromAddress: string,
+    toAddress: string,
+    amount: number,
+    destinationTag?: number
+  ) {
+    const { validatedLedger } = await ripple.getServerInfo();
+
+    const txJSON = {
+      Account: fromAddress,
+      TransactionType: 'Payment',
+      LastLedgerSequence: validatedLedger.ledgerVersion + 4, // not sure of this
+      Destination: toAddress,
+      Amount: ripple.xrpToDrops(amount),
+      DestinationTag: destinationTag,
+    };
+
+    if (!destinationTag) {
+      delete txJSON.DestinationTag;
+    }
+
+    await ripple.prepareTransaction(txJSON);
+    //TODO: send or sign transaction
+    //
+  }
+
+  validateAddress(address: string) {
+    return ripple.isValidAddress(address);
   }
 }
