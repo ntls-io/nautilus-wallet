dc4a2e5f4f1ea6fa284082b7d3d0a7611e8d18cd
Pi Delport <pi@registree.io>
2021-10-05 21:24:18 +0200

    feat(web-server,wallet_operations): begin separating Algorand code

--- web-server/sgx-wallet-impl/src/wallet_operations/sign_transaction.rs
+++ web-server/sgx-wallet-impl/src/wallet_operations/sign_transaction.rs
@@ -1,8 +1,15 @@
-use std::prelude::v1::ToString;
+use std::prelude::v1::*;
 
 use crate::schema::actions::{SignTransaction, SignTransactionResult};
-use crate::schema::entities::AlgorandTransactionSigned;
-use crate::wallet_operations::errors;
+use crate::schema::entities::{
+    AlgorandAccount,
+    AlgorandTransactionSigned,
+    XrpAccount,
+    XrpTransactionSigned,
+};
+use crate::schema::types::Bytes;
+use crate::wallet_operations::sign_transaction_algorand::sign_algorand;
+use crate::wallet_operations::sign_transaction_xrp::sign_xrp;
 use crate::wallet_operations::store::load_wallet;
 
 type Result = SignTransactionResult;
@@ -25,100 +32,47 @@ pub fn sign_transaction(request: &SignTransaction) -> SignTransactionResult {
 
     // Authenticated! Proceed to sign the transaction.
 
-    // Safety: `split_at` panics if len < mid.
-    if request.algorand_transaction_bytes.len() < 2 {
-        return Result::Failed(errors::message_with_base64(
-            "sign_transaction",
-            "transaction too short",
-            format!("len = {}", request.algorand_transaction_bytes.len()),
-            "unsigned transaction msgpack",
-            &request.algorand_transaction_bytes,
-        ));
+    // TODO(Pi): Dispatch this better
+    match (
+        &request.algorand_transaction_bytes,
+        &request.xrp_transaction_bytes,
+    ) {
+        (Some(algorand_transaction_bytes), None) => {
+            sign_algorand_wrapper(&stored.algorand_account, algorand_transaction_bytes)
+        }
+        (None, Some(xrp_transaction_bytes)) => {
+            sign_xrp_wrapper(&stored.xrp_account, xrp_transaction_bytes)
+        }
+        (Some(_), Some(_)) => Result::Failed(ERROR_BOTH.to_string()),
+        (None, None) => Result::Failed(ERROR_NEITHER.to_string()),
     }
-
-    // Check and strip the "TX" prefix tag
-    let transaction_bytes_without_prefix = match request.algorand_transaction_bytes.split_at(2) {
-        (b"TX", rest) => rest,
-        (unrecognised, _rest) => {
-            return Result::Failed(errors::message_with_base64(
-                "sign_transaction",
-                "transaction prefix tag not recognised",
-                format!("expected TX, got {:?}", unrecognised),
-                "unsigned transaction msgpack",
-                &request.algorand_transaction_bytes,
-            ))
-        }
-    };
-
-    let transaction_to_sign =
-        &match algorand_network_compatible::from_msgpack(transaction_bytes_without_prefix) {
-            Ok(ok) => ok,
-            Err(err) => {
-                return Result::Failed(errors::message_with_base64(
-                    "sign_transaction",
-                    "failed to unpack received transaction-to-be-signed",
-                    err,
-                    "unsigned transaction msgpack",
-                    &request.algorand_transaction_bytes,
-                ))
-            }
-        };
-
-    let signing_account = &stored.algorand_account.as_algonaut_account();
-
-    let signed_transaction = &match signing_account.sign_transaction(transaction_to_sign) {
-        Ok(ok) => ok,
-        Err(err) => {
-            return Result::Failed(errors::message_with_debug_value(
-                "sign_transaction",
-                "algonaut error while signing transaction",
-                err,
-                "unsigned transaction",
-                transaction_to_sign,
-            ))
-        }
-    };
-
-    // Note: Intentionally serialize with algonaut's `to_msg_pack` helper, not ours.
-    let signed_transaction_bytes = match algorand_network_compatible::to_msgpack(signed_transaction)
-    {
-        Ok(ok) => ok,
-        Err(err) => {
-            return Result::Failed(errors::message_with_debug_value(
-                "sign_transaction",
-                "failed to pack signed ",
-                err,
-                "signed transaction msgpack",
-                signed_transaction,
-            ))
-        }
-    };
-
-    Result::Signed(AlgorandTransactionSigned {
-        signed_transaction_bytes,
-    })
 }
 
-/// Algorand network-compatible MessagePack serialization and deserialization.
-///
-/// This is important to get right: variations will lead to data that's not valid for Algorand.
-pub mod algorand_network_compatible {
-    use std::prelude::v1::{Box, Vec};
+const ERROR_BOTH: &str =
+    "sign_transaction: cannot specify both algorand_transaction_bytes and xrp_transaction_bytes";
 
-    use algonaut::core::ToMsgPack;
-    use serde::Deserialize;
+const ERROR_NEITHER: &str =
+    "sign_transaction: must specify one of algorand_transaction_bytes or xrp_transaction_bytes";
 
-    /// For serialization, we use algonaut's [`ToMsgPack`] trait.
-    pub fn to_msgpack(value: &impl ToMsgPack) -> Result<Box<[u8]>, rmp_serde::encode::Error> {
-        value.to_msg_pack().map(Vec::into_boxed_slice)
+fn sign_algorand_wrapper(
+    signing_account: &AlgorandAccount,
+    transaction_bytes: &Bytes,
+) -> SignTransactionResult {
+    match sign_algorand(signing_account, transaction_bytes) {
+        Ok(signed_transaction_bytes) => Result::Signed(AlgorandTransactionSigned {
+            signed_transaction_bytes,
+        }),
+        Err(message) => Result::Failed(message),
     }
+}
 
-    /// For deserialization, we use `rmp_serde::from_read_ref`, which algonaut currently uses.
-    pub fn from_msgpack<'de, D, R>(bytes: &'de R) -> Result<D, rmp_serde::decode::Error>
-    where
-        D: Deserialize<'de>,
-        R: AsRef<[u8]> + ?Sized,
-    {
-        rmp_serde::from_read_ref(bytes)
-    }
+fn sign_xrp_wrapper(
+    signing_account: &XrpAccount,
+    transaction_bytes: &Bytes,
+) -> SignTransactionResult {
+    let signature_bytes = sign_xrp(signing_account, transaction_bytes);
+    Result::SignedXrp(XrpTransactionSigned {
+        signed_transaction_bytes: transaction_bytes.to_owned(),
+        signature_bytes,
+    })
 }
