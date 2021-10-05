4f180fad6be1f8961beab0eb5ec0e8e8d84ed5bb
Pi Delport <pi@registree.io>
2021-10-05 15:55:16 +0200

    feat(web-server,schema): add XRP definitions

--- web-server/sgx-wallet-impl/src/schema/entities.rs
+++ web-server/sgx-wallet-impl/src/schema/entities.rs
@@ -1,9 +1,9 @@
 //! Structures representing various entities.
 
-use std::prelude::v1::{String, ToString};
+use std::prelude::v1::{String, ToOwned, ToString};
 
 use algonaut::transaction::account::Account as AlgonautAccount;
-use ripple_keypairs::Seed;
+use ripple_keypairs::{Entropy, EntropyArray, PrivateKey, PublicKey, Seed};
 use secrecy::zeroize::Zeroize;
 use serde::{Deserialize, Serialize};
 
@@ -15,8 +15,9 @@ use crate::schema::types::{
     Bytes,
     WalletId,
     WalletPin,
-    XrpAccountSeedBytes,
-    XrpAddressClassic,
+    XrpAddressBase58,
+    XrpKeyType,
+    XrpPublicKeyHex,
 };
 
 derive_schema_traits!(
@@ -26,8 +27,11 @@ derive_schema_traits!(
     pub struct WalletDisplay {
         pub wallet_id: WalletId,
         pub owner_name: String,
+
+        // TODO(Pi): Decouple for multiple accounts per wallet.
         pub algorand_address_base32: AlgorandAddressBase32,
-        pub xrp_address_classic: XrpAddressClassic,
+
+        pub xrp_account: XrpAccountDisplay,
     }
 );
 
@@ -37,7 +41,7 @@ impl From<WalletStorable> for WalletDisplay {
             wallet_id: storable.wallet_id.clone(),
             owner_name: storable.owner_name.clone(),
             algorand_address_base32: storable.algorand_account.address_base32(),
-            xrp_address_classic: storable.xrp_account.address_classic(),
+            xrp_account: storable.xrp_account.clone().into(),
         }
     }
 }
@@ -55,17 +59,12 @@ derive_schema_traits!(
     }
 );
 
+// Algorand entities:
+
 derive_schema_traits!(
     /// An Algorand account.
     pub struct AlgorandAccount {
         pub seed_bytes: AlgorandAccountSeedBytes,
-    }
-);
-
-derive_schema_traits!(
-    /// An XRP account.
-    pub struct XrpAccount {
-        pub seed_bytes: XrpAccountSeedBytes,
     }
 );
 
@@ -97,19 +96,6 @@ impl From<AlgorandAccount> for AlgonautAccount {
     }
 }
 
-impl XrpAccount {
-    pub(crate) fn generate() -> Self {
-        Self {
-            seed_bytes: Seed::random(),
-        }
-    }
-
-    pub fn address_classic(&self) -> XrpAddressClassic {
-        let (_, public_key) = self.seed_bytes.derive_keypair().unwrap();
-        public_key.derive_address()
-    }
-}
-
 derive_schema_traits!(
     /// An unsigned Algorand transaction.
     pub struct AlgorandTransaction {
@@ -119,9 +105,114 @@ derive_schema_traits!(
 );
 
 derive_schema_traits!(
-    /// An signed Algorand transaction.
+    /// A signed Algorand transaction.
     pub struct AlgorandTransactionSigned {
         #[serde(with = "serde_bytes")]
         pub signed_transaction_bytes: Bytes,
+    }
+);
+
+// XRP entities:
+
+derive_schema_traits!(
+    /// An XRP account's displayable details.
+    pub struct XrpAccountDisplay {
+        pub key_type: XrpKeyType,
+        pub public_key_hex: XrpPublicKeyHex,
+        pub address_base58: XrpAddressBase58,
+    }
+);
+
+impl From<XrpAccount> for XrpAccountDisplay {
+    fn from(xrp_account: XrpAccount) -> Self {
+        Self {
+            key_type: xrp_account.key_type,
+            public_key_hex: xrp_account.to_public_key_hex(),
+            address_base58: xrp_account.to_address_base58(),
+        }
+    }
+}
+
+derive_schema_traits!(
+    /// An XRP account.
+    pub struct XrpAccount {
+        pub key_type: XrpKeyType,
+        pub seed_bytes: EntropyArray,
+    }
+);
+
+impl From<Seed> for XrpAccount {
+    fn from(seed: Seed) -> Self {
+        let key_type = seed.as_kind().into();
+        let seed_bytes = seed.as_entropy().to_owned();
+        Self::new(key_type, seed_bytes)
+    }
+}
+
+impl From<XrpAccount> for Seed {
+    fn from(xrp_account: XrpAccount) -> Self {
+        let entropy = Entropy::Array(xrp_account.seed_bytes);
+        let kind = xrp_account.key_type.into();
+        Seed::new(entropy, kind)
+    }
+}
+
+impl XrpAccount {
+    pub(crate) fn new(key_type: XrpKeyType, seed_bytes: EntropyArray) -> Self {
+        Self {
+            key_type,
+            seed_bytes,
+        }
+    }
+
+    /// Generate a new keypair of the given type.
+    pub(crate) fn generate_default() -> Self {
+        Self::generate(XrpKeyType::default())
+    }
+
+    /// Generate a new keypair of the given type.
+    pub(crate) fn generate(key_type: XrpKeyType) -> Self {
+        Seed::new(Entropy::Random, key_type.into()).into()
+    }
+
+    pub(crate) fn to_seed(&self) -> Seed {
+        self.clone().into()
+    }
+
+    pub(crate) fn to_keypair(&self) -> (PrivateKey, PublicKey) {
+        // XXX(Pi): Performance: Repeated key derivation?
+        self.to_seed()
+            .derive_keypair()
+            // Safety: This should not fail unless something is seriously wrong: treat as unrecoverable.
+            .expect("XrpAccount: derive_keypair failed!")
+    }
+
+    pub(crate) fn to_private_key(&self) -> PrivateKey {
+        let (private_key, _) = self.to_keypair();
+        private_key
+    }
+
+    pub(crate) fn to_public_key(&self) -> PublicKey {
+        let (_, public_key) = self.to_keypair();
+        public_key
+    }
+
+    pub fn to_address_base58(&self) -> XrpAddressBase58 {
+        self.to_public_key().derive_address()
+    }
+
+    // TODO: Support X-Address format? Docs: <https://xrpaddress.info/>
+
+    pub fn to_public_key_hex(&self) -> XrpPublicKeyHex {
+        self.to_public_key().to_string()
+    }
+}
+
+derive_schema_traits!(
+    /// A signed XRP transaction.
+    pub struct XrpTransactionSigned {
+        #[serde(with = "serde_bytes")]
+        pub signed_transaction_bytes: Bytes,
+        pub signature_bytes: Bytes,
     }
 );
