59a49e4df7f0a1e229994c03e2d52818977af351
Pi Delport <pi@registree.io>
2021-10-05 21:24:54 +0200

    feat(web-server,wallet_operations): add XRP code

--- web-server/sgx-wallet-impl/src/wallet_operations/mod.rs
+++ web-server/sgx-wallet-impl/src/wallet_operations/mod.rs
@@ -6,4 +6,5 @@ pub(crate) mod errors;
 pub mod open_wallet;
 pub mod sign_transaction;
 pub mod sign_transaction_algorand;
+pub(crate) mod sign_transaction_xrp;
 pub mod store;
---
+++ web-server/sgx-wallet-test/enclave/src/wallet_operations/test_sign_transaction_xrp.rs
@@ -0,0 +1,30 @@
+use std::prelude::v1::ToString;
+
+use sgx_wallet_impl::schema::actions;
+use sgx_wallet_impl::schema::actions::SignTransactionResult;
+use sgx_wallet_impl::wallet_operations::sign_transaction::sign_transaction;
+
+use crate::helpers::wallet_store::create_test_wallet;
+
+type Result = SignTransactionResult;
+
+pub(crate) fn sign_transaction_empty() {
+    let existing = &create_test_wallet();
+
+    let xrp_transaction_bytes = Default::default();
+
+    let request = &actions::SignTransaction {
+        wallet_id: existing.wallet_id.clone(),
+        auth_pin: "123456".to_string(),
+        algorand_transaction_bytes: None,
+        xrp_transaction_bytes: Some(xrp_transaction_bytes),
+    };
+    let signature = match sign_transaction(request) {
+        Result::SignedXrp(signature) => signature,
+        otherwise => panic!("unexpected: {:?}", otherwise),
+    };
+    let signature_bytes = signature.signature_bytes.as_ref();
+
+    // TODO(Pi): Test more substantially.
+    assert_eq!(signature_bytes[0], 0x30); // DER tag for SEQUENCE
+}
