135840b61cacb801a9a341c5d40057cab5ad3dfe
Pi Delport <pi@registree.io>
2021-10-05 15:56:21 +0200

    feat(web-server,wallet_operations): generate XrpAccount

--- web-server/sgx-wallet-impl/src/wallet_operations/create_wallet.rs
+++ web-server/sgx-wallet-impl/src/wallet_operations/create_wallet.rs
@@ -1,19 +1,22 @@
 use std::prelude::v1::ToString;
 
 use crate::schema::actions::{CreateWallet, CreateWalletResult};
-use crate::schema::entities::{AlgorandAccount, XrpAccount, WalletDisplay, WalletStorable};
+use crate::schema::entities::{AlgorandAccount, WalletDisplay, WalletStorable, XrpAccount};
 use crate::wallet_operations::store::save_new_wallet;
 
 type Result = CreateWalletResult;
 
 pub fn create_wallet(request: &CreateWallet) -> Result {
-    let new_account = AlgorandAccount::generate();
-    let new_xrp_account = XrpAccount::generate();
+    // TODO(Pi): Pull account / keypair creation into a separate operation.
+    //           For now, just generate both Algorand and XRP keypairs.
+    let new_algorand_account = AlgorandAccount::generate();
+    let new_xrp_account = XrpAccount::generate_default();
+
     let storable = WalletStorable {
-        wallet_id: new_account.address_base32(),
+        wallet_id: new_algorand_account.address_base32(),
         owner_name: request.owner_name.clone(),
         auth_pin: request.auth_pin.clone(),
-        algorand_account: new_account,
+        algorand_account: new_algorand_account,
         xrp_account: new_xrp_account,
     };
     match save_new_wallet(&storable) {
