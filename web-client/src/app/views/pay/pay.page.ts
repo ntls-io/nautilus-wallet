693c29ac8a58ae33ae12fb95fcd68c605e4c84d9
Jonathan Ngbonga <jongbonga@gmail.com>
2021-10-06 13:32:11 +0200

    refactor(web-client): use XRP instead of Rands

--- web-client/src/app/views/pay/pay.page.ts
+++ web-client/src/app/views/pay/pay.page.ts
@@ -20,7 +20,7 @@ export class PayPage implements OnInit {
     groupSeparator: ',',
     digits: 2,
     digitsOptional: false,
-    prefix: 'R ',
+    prefix: 'XRP ',
     placeholder: '0',
     autoUnmask: true,
   });
@@ -66,7 +66,7 @@ export class PayPage implements OnInit {
       const { transactionId } = this.sessionQuery.getValue();
 
       this.notifySuccess(
-        'R' + this.paymentForm.controls.amount.value,
+        'XRP' + this.paymentForm.controls.amount.value,
         this.wallet,
         transactionId,
         new Date()
