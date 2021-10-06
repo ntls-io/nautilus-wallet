3b8b3c74aad79d37c547030c066c0ff30737e133
Pi Delport <pi@registree.io>
2021-10-07 00:04:52 +0200

    xxx(web-client): temporarily disable address validation checks

--- web-client/src/app/views/wallet-access/wallet-access.page.ts
+++ web-client/src/app/views/wallet-access/wallet-access.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
 import { LoadingController, ModalController } from '@ionic/angular';
-import { isValidAddress } from 'algosdk';
 import { WalletService } from 'src/app/services/wallet/wallet.service';
 import { SessionQuery } from 'src/app/stores/session';
 import { SwalHelper } from 'src/app/utils/notification/swal-helper';
@@ -42,7 +41,7 @@ export class WalletAccessPage implements OnInit {
   async confirm(value: string | undefined) {
     const address = value?.trim();
 
-    if (address && isValidAddress(address)) {
+    if (address /* XXX(Pi)  && isValidAddress(address)*/) {
       const pinPromise = this.presentLock();
       const loading = await this.loadingCtrl.create();
       const pin = await pinPromise;
