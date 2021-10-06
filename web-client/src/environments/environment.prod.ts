c2b18b0682b03e487de591a5711be508f000b020
Pi Delport <pi@registree.io>
2021-10-06 21:54:25 +0200

    chore(web-client): update API endpoints: ntls-api → xrpl-api

--- web-client/src/environments/environment.prod.ts
+++ web-client/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // TODO(Pi): Use a modal switch for the choice of ledger to use, for now.
   ledger: 'XRP' as 'Algorand' | 'XRP',
   // TODO: Production endpoint
-  nautilusWalletServer: 'https://ntls-api.registree.io/',
+  nautilusWalletServer: 'https://xrpl-api.registree.io/',
   nautilusAssetServices: 'https://ntls-services.registree.io/',
   algod: {
     baseServer: 'https://testnet-algorand.api.purestake.io/ps2',
