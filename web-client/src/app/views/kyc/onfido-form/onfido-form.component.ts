3251f0976d2bdc1ce26ae7c9795bb3f2436e9c40
Pi Delport <pi@registree.io>
2021-10-11 03:59:31 +0200

    chore(web-client,onfido-form): flesh out component, add story

--- web-client/src/app/views/kyc/onfido-form/onfido-form.component.ts
+++ web-client/src/app/views/kyc/onfido-form/onfido-form.component.ts
@@ -1,14 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
+export type OnfidoFormValue = {
+  firstName: string;
+  lastName: string;
+};
+
+/**
+ * Form to collect the details to start the Onfido process.
+ */
 @Component({
   selector: 'app-onfido-form',
   templateUrl: './onfido-form.component.html',
   styleUrls: ['./onfido-form.component.scss'],
 })
 export class OnfidoFormComponent implements OnInit {
+  // Initial first name.
+  @Input() firstName = '';
 
-  constructor() { }
+  // Initial last name.
+  @Input() lastName = '';
 
-  ngOnInit() {}
+  //
+  @Output() submitted = new EventEmitter<OnfidoFormValue>();
 
+  nameForm = new FormGroup({
+    firstName: nameControl(),
+    lastName: nameControl(),
+  });
+
+  constructor() {}
+
+  ngOnInit() {
+    // Set initial values.
+    this.nameForm.setValue({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    });
+  }
+
+  // Emit `this.submitted`
+  onSubmit() {
+    if (this.nameForm.valid) {
+      const value: OnfidoFormValue = this.nameForm.value;
+      const { firstName, lastName } = value;
+      const trimmed = {
+        firstName: firstName.trim(),
+        lastName: lastName.trim(),
+      };
+      this.submitted.emit(trimmed);
+    }
+  }
 }
+
+// FormControl for names: non-blank strings.
+const nameControl = () => new FormControl('', [Validators.required, notBlank]);
+
+// Validator: Reject blank, whitespace-only strings.
+const notBlank: ValidatorFn = (control) =>
+  control.value.trim() === '' ? { notEmpty: true } : null;
