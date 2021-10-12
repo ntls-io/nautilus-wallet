0ba373c50954deb2ac8890186475b5304be1e46d
Pi Delport <pi@registree.io>
2021-10-12 21:12:05 +0200

    feat(web-client,onfido.service): tweak, add createCheck / retrieveCheck

--- web-client/src/app/stores/onfido/onfido.service.ts
+++ web-client/src/app/stores/onfido/onfido.service.ts
@@ -1,17 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { panic } from 'src/app/utils/errors/panic';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class OnfidoService {
   constructor(private http: HttpClient) {}
 
-  async getToken(first_name: string, last_name: string) {
+  async start(
+    first_name: string,
+    last_name: string
+  ): Promise<OnfidoKycStarted> {
     const url = this.getAssetServicesUrl('kyc/start');
-    const body = { first_name, last_name };
+    const body: StartKyc = { first_name, last_name };
     const response = await this.http.post(url, body).toPromise();
-    const { sdk_token } = response as { sdk_token: string };
-    return sdk_token;
+    return checkedOnfidoKycStarted(response);
+  }
+
+  async createCheck(
+    applicant_id: string,
+    report_names: Array<string>
+  ): Promise<Check> {
+    const url = this.getAssetServicesUrl('kyc/checks/create');
+    const body: CreateCheck = { applicant_id, report_names };
+    const response = await this.http.post(url, body).toPromise();
+    return response as Check; // XXX: Unchecked cast
+  }
+
+  async retrieveCheck(check_id: string): Promise<Check> {
+    const url = this.getAssetServicesUrl('kyc/checks/retrieve');
+    const body: RetrieveCheck = { check_id };
+    const response = await this.http.post(url, body).toPromise();
+    return response as Check; // XXX: Unchecked cast
   }
 
   protected getAssetServicesUrl(path: string): string {
@@ -23,3 +43,55 @@ export class OnfidoService {
     }
   }
 }
+
+const checkedOnfidoKycStarted = (response: any): OnfidoKycStarted => {
+  const { applicant_id, sdk_token } = response;
+  if (typeof applicant_id === 'string' && typeof sdk_token == 'string') {
+    return { applicant_id, sdk_token };
+  } else {
+    throw panic('OnfidoService.start: unexpected response:', response);
+  }
+};
+
+type StartKyc = {
+  first_name: string;
+  last_name: string;
+};
+
+export type OnfidoKycStarted = {
+  applicant_id: string;
+  sdk_token: string;
+};
+
+type CreateCheck = {
+  applicant_id: string;
+  report_names: Array<string>;
+};
+
+type RetrieveCheck = {
+  check_id: string;
+};
+
+export type Check = {
+  id?: string;
+  created_at?: string;
+  href?: string;
+  status?: string;
+  result?: string;
+  download_uri?: string;
+  form_uri?: string;
+  redirect_uri?: string;
+  results_uri?: string;
+  report_names?: Array<string>;
+  applicant_id?: string;
+  privacy_notices_read_consent_given?: boolean;
+  tags?: Array<string>;
+  applicant_provides_data?: boolean;
+  suppress_form_emails?: boolean;
+  asynchronous?: boolean;
+  webhook_ids?: Array<string>;
+  report_ids?: Array<string>;
+  document_ids?: Array<string>;
+  consider?: Array<string>;
+  sub_result?: string;
+};
