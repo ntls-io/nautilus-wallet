f93eb4048f34eb5c3f5ed5fdbef50ff372e7803e
Pi Delport <pi@registree.io>
2021-10-25 15:10:39 +0200

    feat(asset-services,onfido): CORS and tweaks (#127)
    * chore(asset-services,Cargo): use tower-http branch "cors"
    
    Upstream PR:
    
    - <https://github.com/tower-rs/tower-http/pull/112>
    
    * feat(asset-services-api): add permissive CORS layer
    
    * feat(asset-services-api,kyc): bump default task timeout: 5 → 15
    
    (Some requests take 5–10 seconds.)

--- asset-services/asset-services-api/src/main.rs
+++ asset-services/asset-services-api/src/main.rs
@@ -10,6 +10,7 @@ use axum::handler::post;
 use axum::{AddExtensionLayer, Router};
 
 use crate::helpers::bind_addr_from_env;
+use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -20,6 +21,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let celery = Arc::new(init_celery_from_env().await?);
     celery.display_pretty().await;
 
+    // CORS:
+    let cors_layer = CorsLayer::permissive();
+
     let axum_app = Router::new()
         .route("/ping", post(handlers::misc::ping))
         .route("/verify/start", post(handlers::verification::start_verify))
@@ -27,7 +31,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/kyc/start", post(handlers::kyc::start_kyc))
         .route("/kyc/checks/create", post(handlers::kyc::create_check))
         .route("/kyc/checks/retrieve", post(handlers::kyc::retrieve_check))
-        .layer(AddExtensionLayer::new(celery.clone()));
+        .layer(AddExtensionLayer::new(celery.clone()))
+        // The CORS layer must come after the wrapped resources, for correct response headers.
+        .layer(cors_layer);
     let axum_server = axum::Server::bind(&bind_addr).serve(axum_app.into_make_service());
     log::info!("listening on http://{}", bind_addr);
     axum_server.await?;
