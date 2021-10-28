97535559ec73e0b179d4d1adbffb036495cdca0c
Pi Delport <pi@registree.io>
2021-10-28 09:08:55 +0200

    test(web-client,modal.helpers): better interface: withStubbedModal / withStubbedModalScanner

--- web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
+++ web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
@@ -2,10 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule, ModalController } from '@ionic/angular';
-import {
-  expectModalScannerPresented,
-  stubModalScannerResult,
-} from 'src/tests/modal.helpers.spec';
+import { withStubbedModalScanner } from 'src/tests/modal.helpers.spec';
 import Swal, { SweetAlertOptions, SweetAlertResult } from 'sweetalert2';
 import { WalletAccessPage } from './wallet-access.page';
 
@@ -37,30 +34,33 @@ describe('WalletAccessPage', () => {
   });
 
   it('#openScanner should fire alert if camera permission request fails', async () => {
-    const modalScannerSpies = stubModalScannerResult(modalCtrl, {
-      type: 'permissionDenied',
-    });
-
     // TODO: Implement this by wrapping swal in a service or using ngx-sweetalert2
 
     const swalSpy: jasmine.Spy<SwalFire> = spyOn(Swal, 'fire');
-    await component.openScanner();
+
+    await withStubbedModalScanner(
+      modalCtrl,
+      { type: 'permissionDenied' },
+      async () => {
+        await component.openScanner();
+      }
+    );
 
     expect(swalSpy).toHaveBeenCalledOnceWith({
       icon: 'error',
       title: 'Permission required',
       text: `In order to scan a QR Code, you need to grant camera's permission`,
     });
-    expectModalScannerPresented(modalScannerSpies);
   });
 
   it('#openScanner should create the a modal with the scanner page if camera permission request succeeds', async () => {
-    const modalScannerSpies = stubModalScannerResult(modalCtrl, {
-      type: 'dismissed',
-    });
-
-    await component.openScanner();
-    expectModalScannerPresented(modalScannerSpies);
+    await withStubbedModalScanner(
+      modalCtrl,
+      { type: 'dismissed' },
+      async () => {
+        await component.openScanner();
+      }
+    );
   });
 });
 
--- web-client/src/tests/modal.helpers.spec.ts
+++ web-client/src/tests/modal.helpers.spec.ts
@@ -1,30 +1,64 @@
 import { ModalController } from '@ionic/angular';
+import { ComponentRef } from '@ionic/core';
 import { ScannerPage, ScanResult } from 'src/app/views/scanner/scanner.page';
 
-export type ModalScannerSpies = {
+export type ModalSpies = {
   modalCreateSpy: jasmine.Spy<ModalController['create']>;
   modalSpy: jasmine.SpyObj<HTMLIonModalElement>;
 };
 
-/** Stub {@link ModalController.create} with a `ScannerPage` modal that returns `scanResult` on dismiss.  */
-export const stubModalScannerResult = (
+/** Stub {@link ModalController.create} with a modal that returns `dismissResult` on dismiss.  */
+export const stubModalResult = <R>(
   modalCtrl: ModalController,
-  scanResult: ScanResult
-): ModalScannerSpies => {
+  dismissResult: R
+): ModalSpies => {
   const modalSpy = jasmine.createSpyObj<HTMLIonModalElement>('Modal', {
     present: Promise.resolve(),
-    onDidDismiss: Promise.resolve({ data: scanResult }),
+    onDidDismiss: Promise.resolve({ data: dismissResult }),
   });
-  const modalCreateSpy = spyOn(modalCtrl, 'create')
-    .withArgs({ component: ScannerPage })
-    .and.resolveTo(modalSpy);
+  const modalCreateSpy = spyOn(modalCtrl, 'create').and.resolveTo(modalSpy);
   return { modalCreateSpy, modalSpy };
 };
 
-/** Expect the `ScannerPage` modal to have been presented. */
-export const expectModalScannerPresented = (spies: ModalScannerSpies) => {
+/** Expect a modal with the given component to have been presented. */
+export const expectModalComponentPresented = (
+  spies: ModalSpies,
+  expectedComponent: ComponentRef
+): void => {
   expect(spies.modalCreateSpy).toHaveBeenCalledOnceWith({
-    component: ScannerPage,
+    component: expectedComponent,
   });
   expect(spies.modalSpy.present).toHaveBeenCalledOnceWith();
+};
+
+/**
+ * Wrap `f` with the given stubbed modal component and result.
+ */
+export const withStubbedModal = async <R>(
+  modalCtrl: ModalController,
+  expectedComponent: ComponentRef,
+  dismissResult: R,
+  f: () => Promise<void>
+): Promise<void> => {
+  const modalSpies = stubModalResult(modalCtrl, dismissResult);
+  await f();
+  expectModalComponentPresented(modalSpies, expectedComponent);
+};
+
+/**
+ * Convenience helper: `withStubbedModal` for `ScannerPage`.
+ */
+export const withStubbedModalScanner = async (
+  modalCtrl: ModalController,
+  dismissResult: ScanResult,
+  f: () => Promise<void>
+): Promise<void> => {
+  await withStubbedModal<ScanResult>(
+    modalCtrl,
+    ScannerPage,
+    dismissResult,
+    async () => {
+      await f();
+    }
+  );
 };
