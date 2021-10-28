dc52b0321dfcdb5611da29fb1e5678ad1e4acc67
Pi Delport <pi@registree.io>
2021-10-28 19:07:28 +0200

    feat(web-client,SessionQuery): use asset balance for balance query

--- web-client/src/app/stores/session/session.query.ts
+++ web-client/src/app/stores/session/session.query.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
+import { extractAlgorandAssetBalance } from 'src/app/services/algosdk.utils';
+import { environment } from 'src/environments/environment';
 import { SessionState, SessionStore } from './session.store';
 
 @Injectable({
@@ -8,7 +10,16 @@ import { SessionState, SessionStore } from './session.store';
 export class SessionQuery extends Query<SessionState> {
   name = this.select('name');
   walletId = this.select('walletId');
-  balance = this.select('balance');
+
+  // XXX(Pi): Just use Algorand asset balance, for now.
+  balance = this.select((state) =>
+    state.algorandAccount
+      ? extractAlgorandAssetBalance(
+          state.algorandAccount,
+          environment.defaultAlgorandAssetId
+        )
+      : undefined
+  );
 
   constructor(protected store: SessionStore) {
     super(store);
