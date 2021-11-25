7897c4d023012746c4ec0728245901c33df9748f
Jonathan Ngbonga <jongbonga@gmail.com>
2021-11-25 23:44:15 +0200

    refactor(web-client): move payfromto to shared mod

--- web-client/src/app/views/pay/pay.module.ts
+++ web-client/src/app/views/pay/pay.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { InputMaskModule } from '@ngneat/input-mask';
-import { PayFromToComponent } from 'src/app/components/pay-from-to/pay-from-to.component';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { PayPageRoutingModule } from './pay-routing.module';
 import { PayPage } from './pay.page';
@@ -18,6 +17,6 @@ import { PayPage } from './pay.page';
     SharedModule,
     InputMaskModule.forRoot({ inputSelector: 'input', isAsync: true }),
   ],
-  declarations: [PayPage, PayFromToComponent],
+  declarations: [PayPage],
 })
 export class PayPageModule {}
