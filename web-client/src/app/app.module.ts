b7927ec988cf0dd67449d7dae3116708383afac1
Jonathan Ngbonga <jongbonga@gmail.com>
2021-12-08 22:34:16 +0200

    refactor(web-client): move sentry to global error handler

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -1,28 +1,17 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import * as SentryAngular from '@sentry/angular';
-import * as Sentry from '@sentry/capacitor';
-import { Integrations as TracingIntegrations } from '@sentry/tracing';
 import { NgxPrinterModule } from 'ngx-printer';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorHandlerModule } from './modules/error-handler.module';
 import { TranslocoRootModule } from './transloco/transloco-root.module';
-
-Sentry.init(
-  {
-    dsn: 'https://67b1d83771ef47bfb176012e478f8a6f@o1082240.ingest.sentry.io/6090433',
-    integrations: [new TracingIntegrations.BrowserTracing()],
-  },
-  SentryAngular.init
-);
 
 @NgModule({
   declarations: [AppComponent],
@@ -39,13 +28,7 @@ Sentry.init(
     NgxPrinterModule.forRoot({ printOpenWindow: false }),
     ErrorHandlerModule,
   ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {
-      provide: ErrorHandler,
-      useValue: SentryAngular.createErrorHandler(),
-    },
-  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
