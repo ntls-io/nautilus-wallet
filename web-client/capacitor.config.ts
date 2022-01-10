00d65a6ce0b84c423885bc40fec92ef63558460e
Jonathan Ngbonga <jongbonga@gmail.com>
2022-01-10 14:42:23 +0200

    chore(web-client): update appId domain

--- web-client/capacitor.config.ts
+++ web-client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ntls.wallet',
   appName: 'Nautilus wallet',
   webDir: 'www',
   bundledWebRuntime: false,
