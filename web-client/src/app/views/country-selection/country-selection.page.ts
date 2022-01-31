ec8dac5094a492a9ad46e798c131221c2beb26c1
Jonathan Ngbonga <jonathan@ntls.io>
2022-01-31 16:50:39 +0200

    rever(web-client): revert 3da8856

--- web-client/src/app/views/country-selection/country-selection.page.ts
+++ web-client/src/app/views/country-selection/country-selection.page.ts
@@ -21,13 +21,13 @@ export class CountrySelectionPage implements OnInit {
   async getCountries() {
     this.isBusy = true;
     await this.http
-      .get('https://restcountries.com/v2/all?fields=name,flags,callingCodes')
+      .get('https://restcountries.com/v3.1/all?fields=name,flags,idd')
       .toPromise()
       .then(
         (data: any) => {
           this.countries = data.sort(
-            (a: { name: string }, b: { name: string }) =>
-              a.name.localeCompare(b.name)
+            (a: { name: { common: string } }, b: { name: { common: any } }) =>
+              a.name.common.localeCompare(b.name.common)
           );
           this.filteredCountries = this.countries;
         },
@@ -41,8 +41,13 @@ export class CountrySelectionPage implements OnInit {
   }
 
   dismiss(country: any) {
+    console.log(country);
+    const suffix =
+      country?.idd?.root === '+1' ? '' : country?.idd?.suffixes[0]?.slice(0, 2);
+    const prefix = country ? country?.idd?.root + suffix : undefined;
+
     this.modalCtrl.dismiss({
-      prefix: country ? '+' + country?.callingCodes[0] : undefined,
+      prefix,
     });
   }
 
@@ -52,7 +57,9 @@ export class CountrySelectionPage implements OnInit {
 
   onSearchChange(event: any) {
     const filtered = this.countries.filter((country) =>
-      country?.name?.toLowerCase().includes(event.target.value.toLowerCase())
+      country?.name?.common
+        ?.toLowerCase()
+        .includes(event.target.value.toLowerCase())
     );
 
     this.filteredCountries = filtered.length > 0 ? filtered : this.countries;
