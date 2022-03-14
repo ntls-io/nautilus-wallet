a4792b81e3b1e40f73e57353568d27f359903f81
Jonathan Ngbonga <12111716+jongbonga@users.noreply.github.com>
2022-03-14 17:11:19 +0200

    refactor(web-client): add inputs to profile card (#138)

--- web-client/src/app/components/profile-card-horizontal/profile-card-horizontal.component.ts
+++ web-client/src/app/components/profile-card-horizontal/profile-card-horizontal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card-horizontal',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-card-horizontal.component.scss'],
 })
 export class ProfileCardHorizontalComponent implements OnInit {
+  @Input() title: string | undefined;
+  @Input() fullName!: string;
+  @Input() cellphone!: string;
+  @Input() avatar = 'assets/img/logo.svg';
+
   constructor() {}
 
   ngOnInit() {}
