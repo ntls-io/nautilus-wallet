cc9ba33aa2b7303f2d2808289e8e237f5a5e3a4c
Pi Delport <pi@ntls.io>
2022-03-20 20:38:50 +0200

    test(web-client): expose PayFromToDefaultArgs for easier arg reuse

--- web-client/src/app/components/pay-from-to/pay-from-to.component.stories.ts
+++ web-client/src/app/components/pay-from-to/pay-from-to.component.stories.ts
@@ -4,15 +4,21 @@ import { PayFromToComponent } from './pay-from-to.component';
 export default {
   title: 'Components/PayFromTo',
   component: PayFromToComponent,
+  excludeStories: ['PayFromToDefaultArgs'],
 } as Meta;
+
+// Export this separately for args reuse, to avoid undesired type widening.
+//
+// (When accessing this through `Default.args`, all fields become `Partial`.)
+export const PayFromToDefaultArgs = {
+  name: 'Test Owner',
+  balance: 1234,
+  receiverAddress: 'G6AIRDAJFSBXNFBHLQ2F5JLZJ6EYYYLDZSCDHUQUB2YUG5QO4ZB4VNAL7I',
+};
 
 const Template: Story<PayFromToComponent> = (args: PayFromToComponent) => ({
   props: args,
 });
 
 export const Default = Template.bind({});
-Default.args = {
-  name: 'Test Owner',
-  balance: 1234,
-  receiverAddress: 'G6AIRDAJFSBXNFBHLQ2F5JLZJ6EYYYLDZSCDHUQUB2YUG5QO4ZB4VNAL7I',
-};
+Default.args = PayFromToDefaultArgs;
