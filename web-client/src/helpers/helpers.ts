79f8b91e48dc8a0ab9c0332217cab8d3f669b823
Pi Delport <pi@ntls.io>
2022-04-11 01:18:10 +0200

    feat(web-client,helpers): add ifDefined helper

--- web-client/src/helpers/helpers.ts
+++ web-client/src/helpers/helpers.ts
@@ -9,3 +9,14 @@ export const never = (value: never): never => {
   console.error('expected never, got:', value);
   throw new TypeError('expected never, got value (see error log)');
 };
+
+/**
+ * Apply `f` to an optional `value`.
+ *
+ * This works like nullish coalescing and optional chaining, but for a function argument.
+ */
+export const ifDefined = <T>(
+  value: T | null | undefined,
+  f: (v: T) => T
+): T | undefined =>
+  value === null || value === undefined ? undefined : f(value);
