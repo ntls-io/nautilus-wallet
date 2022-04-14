9682a4d1914625520d1c4455cd9813a293b4d32f
Pi Delport <pi@ntls.io>
2022-04-14 10:01:01 +0200

    feat(web-client,algorand.helpers): add makeAssetTransferTxnHelper

--- web-client/src/schema/algorand.helpers.spec.ts
+++ web-client/src/schema/algorand.helpers.spec.ts
@@ -1,5 +1,11 @@
-import { SuggestedParams, Transaction } from 'algosdk';
-import { makePaymentTxnHelper, OptionalParameters } from './algorand.helpers';
+import { SuggestedParams, Transaction, TransactionType } from 'algosdk';
+import { AssetTransferTransaction } from 'algosdk/dist/types/src/types/transactions/asset';
+import {
+  AssetTransferRequiredParameters,
+  makeAssetTransferTxnHelper,
+  makePaymentTxnHelper,
+  OptionalParameters,
+} from './algorand.helpers';
 
 describe('makePaymentTxnHelper', () => {
   const suggested: SuggestedParams = {
@@ -60,6 +66,83 @@ describe('makePaymentTxnHelper', () => {
       })
     ).toEqual(
       new Transaction({
+        ...suggested,
+        ...required,
+        lastRound: suggested.firstRound + 4,
+      })
+    );
+  });
+});
+
+describe('makeAssetTransferTxnHelper', () => {
+  const suggested: SuggestedParams = {
+    fee: 1_000,
+    firstRound: 10_000,
+    lastRound: 11_000,
+    genesisID: 'dummy genesis ID',
+    genesisHash: 'dummy genesis hash',
+  };
+  const dummyAddress =
+    'VCMJKWOY5P5P7SKMZFFOCEROPJCZOTIJMNIYNUCKH7LRO45JMJP6UYBIJA';
+
+  const required: AssetTransferRequiredParameters = {
+    from: dummyAddress,
+    to: dummyAddress,
+    amount: 123,
+    assetIndex: 456,
+  };
+
+  /**
+   * For this helper, we expect the "axfer" transaction type.
+   */
+  const newAxferTransaction = (
+    transaction: AssetTransferTransaction
+  ): Transaction =>
+    new Transaction({ type: TransactionType.axfer, ...transaction });
+
+  it('with required only', () => {
+    expect(makeAssetTransferTxnHelper(suggested, required)).toEqual(
+      newAxferTransaction({ ...suggested, ...required })
+    );
+  });
+
+  it('with no-effect optional', () => {
+    for (const optional of [
+      undefined,
+      {},
+      { options: {} },
+      { modifySuggested: (params) => params },
+      { options: {}, modifySuggested: (params) => params },
+    ] as Array<OptionalParameters>) {
+      expect(makeAssetTransferTxnHelper(suggested, required, optional)).toEqual(
+        newAxferTransaction({ ...suggested, ...required })
+      );
+    }
+  });
+
+  it('with options', () => {
+    const note = new TextEncoder().encode('test note');
+    expect(
+      makeAssetTransferTxnHelper(suggested, required, { options: { note } })
+    ).toEqual(
+      newAxferTransaction({
+        ...suggested,
+        ...required,
+        note,
+      })
+    );
+  });
+
+  it('with modifySuggested', () => {
+    expect(
+      makeAssetTransferTxnHelper(suggested, required, {
+        modifySuggested: (params) => ({
+          ...params,
+          lastRound: params.firstRound + 4,
+        }),
+      })
+    ).toEqual(
+      newAxferTransaction({
         ...suggested,
         ...required,
         lastRound: suggested.firstRound + 4,
