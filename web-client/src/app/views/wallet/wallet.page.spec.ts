c44f715894d322f353ca3fdd8447bd42120fe8b4
Pi Delport <pi@ntls.io>
2022-04-14 10:01:03 +0200

    feat(web-client,WalletPage): handle asset opt-ins on init

--- web-client/src/app/views/wallet/wallet.page.spec.ts
+++ web-client/src/app/views/wallet/wallet.page.spec.ts
@@ -34,8 +34,16 @@ describe('WalletPage', () => {
       component = fixture.componentInstance;
 
       // Satisfy OpenWalletGuard
-      stubActiveSession(TestBed.inject(SessionStore), {
+      const sessionStore = TestBed.inject(SessionStore);
+      stubActiveSession(sessionStore, {
         wallet: { owner_name: 'Wallet Owner' },
+      });
+      // For the balance checks:
+      sessionStore.update({
+        algorandAccountData: {
+          address: 'placeholder address',
+          amount: 0,
+        },
       });
 
       fixture.detectChanges();
--- web-client/src/app/views/wallet/wallet.page.ts
+++ web-client/src/app/views/wallet/wallet.page.ts
@@ -9,14 +9,21 @@ import {
   faReceipt,
   faWallet,
 } from '@fortawesome/free-solid-svg-icons';
-import { LoadingController } from '@ionic/angular';
+import {
+  LoadingController,
+  ToastController,
+  ToastOptions,
+} from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SessionAlgorandService } from 'src/app/state/session-algorand.service';
 import { SessionQuery } from 'src/app/state/session.query';
 import { assetAmountAlgo } from 'src/app/utils/assets/assets.algo';
 import { AssetAmount } from 'src/app/utils/assets/assets.common';
+import { defined } from 'src/app/utils/errors/panic';
 import { withLoadingOverlayOpts } from 'src/app/utils/loading.helpers';
+import { SwalHelper } from 'src/app/utils/notification/swal-helper';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-wallet',
@@ -35,7 +42,9 @@ export class WalletPage implements OnInit {
     {
       title: 'Top Up Wallet',
       icon: faDonate,
+      // TODO(Pi): Parameterize this
       url: 'https://testnet.algoexplorer.io/dispenser',
+      disabled: true,
     },
     {
       title: 'Verify Profile',
@@ -69,10 +78,17 @@ export class WalletPage implements OnInit {
   constructor(
     private loadingController: LoadingController,
     public sessionQuery: SessionQuery,
-    public sessionAlgorandService: SessionAlgorandService
+    public sessionAlgorandService: SessionAlgorandService,
+    private toastCtrl: ToastController,
+    private notification: SwalHelper
   ) {}
 
-  ngOnInit() {}
+  /**
+   * When the wallet first displays, perform opportunistic asset opt-in.
+   */
+  async ngOnInit(): Promise<void> {
+    await this.checkAlgorandAssetOptIn();
+  }
 
   async onRefresh(): Promise<void> {
     await withLoadingOverlayOpts(
@@ -82,5 +98,56 @@ export class WalletPage implements OnInit {
         await this.sessionAlgorandService.loadAccountData();
       }
     );
+  }
+
+  /**
+   * Perform opportunistic Algorand asset opt-in.
+   *
+   * - If there's no Algo balance, show a suggestion to deposit.
+   * - If the account has an Algo balance but no asset balance, attempt to send an asset opt-in.
+   */
+  protected async checkAlgorandAssetOptIn(): Promise<void> {
+    if (this.sessionQuery.hasAlgorandBalance()) {
+      const assetId = defined(environment.defaultAlgorandAssetId);
+      if (!this.sessionQuery.hasAlgorandAssetBalance(assetId)) {
+        const sending = await this.toast('Sending asset opt-in…');
+        try {
+          await this.sessionAlgorandService.sendAssetOptIn(assetId);
+        } catch (err) {
+          await sending.dismiss();
+          await this.errorNotification('Asset opt-in failed', err);
+          return;
+        }
+        await this.toast('Asset opt-in successful.');
+      }
+    } else {
+      await this.toast('No account balance. Deposit some Algo to get started.');
+    }
+  }
+
+  protected async toast(
+    message: string,
+    opts?: ToastOptions
+  ): Promise<HTMLIonToastElement> {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 5000,
+      ...opts,
+    });
+    await toast.present();
+    return toast;
+  }
+
+  protected async errorNotification(
+    titleText: string,
+    err: any
+  ): Promise<void> {
+    const text = err?.response?.body?.message ?? err?.response?.body ?? err;
+    console.error('WalletPage.withAlertErrors caught', { err });
+    await this.notification.swal.fire({
+      icon: 'error',
+      titleText,
+      text,
+    });
   }
 }
