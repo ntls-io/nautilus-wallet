a8f44737c791481b9665517c03da6153b46aa20a
Pi Delport <pi@ntls.io>
2022-04-16 21:00:37 +0200

    refactor(web-client,PinEntryComponent): extract and adapt from LockScreenPage

--- web-client/src/app/components/pin-entry/pin-entry.module.ts
+++ web-client/src/app/components/pin-entry/pin-entry.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { PinEntryComponent } from './pin-entry.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule],
   declarations: [PinEntryComponent],
   exports: [PinEntryComponent],
 })
