fc356447b1671e987de6ae19f13af7f0a49ccfa6
Pi Delport <pi@ntls.io>
2022-04-16 21:03:49 +0200

    refactor(web-client,WalletAccessPage): migrate from LockscreenPage to PinEntryComponent

--- web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
+++ web-client/src/app/views/wallet-access/wallet-access.page.spec.ts
@@ -1,15 +1,13 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule, ModalController } from '@ionic/angular';
-import {
-  LockscreenPage,
-  LockscreenResult,
-} from 'src/app/views/lockscreen/lockscreen.page';
-import {
-  withStubbedModal,
-  withStubbedModalScanner,
-} from 'src/tests/modal.helpers.spec';
+import { routes } from 'src/app/app-routing.module';
+import { SessionService } from 'src/app/state/session.service';
+import { SessionStore } from 'src/app/state/session.store';
+import { withStubbedModalScanner } from 'src/tests/modal.helpers.spec';
+import { stubActiveSession } from 'src/tests/state.helpers';
 import Swal, { SweetAlertOptions, SweetAlertResult } from 'sweetalert2';
 import { WalletAccessPageModule } from './wallet-access.module';
 import { WalletAccessPage } from './wallet-access.page';
@@ -17,7 +15,9 @@ import { WalletAccessPage } from './wallet-access.page';
 describe('WalletAccessPage', () => {
   let component: WalletAccessPage;
   let fixture: ComponentFixture<WalletAccessPage>;
+
   let modalCtrl: ModalController;
+  let swalSpy: jasmine.Spy<SwalFire>;
 
   beforeEach(
     waitForAsync(() => {
@@ -26,26 +26,56 @@ describe('WalletAccessPage', () => {
         imports: [
           IonicModule.forRoot(),
           WalletAccessPageModule,
-          RouterTestingModule,
+          RouterTestingModule.withRoutes(routes),
           HttpClientTestingModule,
         ],
       }).compileComponents();
 
       fixture = TestBed.createComponent(WalletAccessPage);
       component = fixture.componentInstance;
-      modalCtrl = TestBed.get(ModalController);
+
+      modalCtrl = TestBed.inject(ModalController);
+      swalSpy = spyOn(Swal, 'fire');
+
       fixture.detectChanges();
     })
   );
+
+  // Check for any unexpected SweetAlert alerts.
+  // (Tests that assert alerts must call swalSpy.calls.reset() after asserting.)
+  afterEach(async () => {
+    await expect(swalSpy).not.toHaveBeenCalled();
+    await expect(swalSpy.calls.allArgs()).toEqual([]); // Better reporting.
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  describe('validatedAddress', () => {
+    type Address = string | undefined;
+    const examples: [Address, Address][] = [
+      // Rejects:
+      [undefined, undefined],
+      ['', undefined],
+      ['  ', undefined],
+      // Accepts:
+      ['address', 'address'],
+      // Trims whitespace:
+      [' address ', 'address'],
+    ];
+    for (const [unvalidated, validated] of examples) {
+      it(`validates ${JSON.stringify(unvalidated)} to ${JSON.stringify(
+        validated
+      )}`, () => {
+        component.address = unvalidated;
+        expect(component.validatedAddress).toBe(validated);
+      });
+    }
+  });
+
   it('#openScanner should fire alert if camera permission request fails', async () => {
     // TODO: Implement this by wrapping swal in a service or using ngx-sweetalert2
-
-    const swalSpy: jasmine.Spy<SwalFire> = spyOn(Swal, 'fire');
 
     await withStubbedModalScanner(
       modalCtrl,
@@ -60,6 +90,7 @@ describe('WalletAccessPage', () => {
       title: 'Permission required',
       text: `In order to scan a QR Code, you need to grant camera's permission`,
     });
+    swalSpy.calls.reset();
   });
 
   it('#openScanner should create the a modal with the scanner page if camera permission request succeeds', async () => {
@@ -72,34 +103,102 @@ describe('WalletAccessPage', () => {
     );
   });
 
-  describe('presentLock', () => {
-    // This test checks that the LockscreenPage and its dependencies load correctly,
-    // to catch things like incorrect imports.
-    it('loads and dismisses', async () => {
-      const promise = component.presentLock();
+  describe('confirm', () => {
+    for (const emptyAddress of ['', ' ']) {
+      it(`rejects empty address: ${JSON.stringify(emptyAddress)}`, () => {
+        const showPinEntryModalSpy = spyOn(component, 'showPinEntryModal');
 
-      // XXX(Pi): We can only call modalCtrl.dismiss() after presentLock() calls present(),
-      //          but we don't have an easy way to wait for that.
-      //          This hacky setTimeout works, for now.
-      //          (500 ms seems to be about the right amount to wait reliably; 100 ms is too low.)
-      setTimeout(async () => {
-        await modalCtrl.dismiss();
-      }, 500);
+        component.address = emptyAddress;
+        component.confirmAddress();
 
-      const { success } = await promise;
-      expect(success).toBe(false);
+        expect(showPinEntryModalSpy).not.toHaveBeenCalled();
+        expect(swalSpy).toHaveBeenCalledOnceWith({
+          icon: 'warning',
+          title: 'Invalid Address',
+          text: 'Please input a valid wallet address',
+        });
+        swalSpy.calls.reset();
+      });
+    }
+
+    it('shows modal dialog', () => {
+      const showPinEntryModalSpy = spyOn(component, 'showPinEntryModal');
+
+      component.address = 'non-empty address';
+      component.confirmAddress();
+
+      expect(showPinEntryModalSpy).toHaveBeenCalledOnceWith();
+    });
+  });
+
+  describe('onPinConfirmed', () => {
+    let router: Router;
+    let sessionService: SessionService;
+    let openWalletSpy: jasmine.Spy<SessionService['openWallet']>;
+
+    beforeEach(async () => {
+      router = TestBed.inject(Router);
+      await router.navigate(['wallet-access']);
+      expect(router.url).toBe('/wallet-access');
+
+      sessionService = TestBed.inject(SessionService);
+      openWalletSpy = spyOn(sessionService, 'openWallet');
     });
 
-    it('returns pin', async () => {
-      await withStubbedModal<LockscreenResult>(
-        modalCtrl,
-        LockscreenPage,
-        { success: true, pin: '1234' },
-        async () => {
-          const { pin } = await component.presentLock();
-          expect(pin).toBe('1234');
-        }
+    afterEach(() => {
+      expect(openWalletSpy).not.toHaveBeenCalled();
+      expect(openWalletSpy.calls.allArgs()).toEqual([]);
+    });
+
+    it('rejects invalid address', async () => {
+      expect(component.validatedAddress).toBeUndefined();
+      await expectAsync(component.onPinConfirmed('')).toBeRejectedWithError(
+        'WalletAccessPage.onPinConfirmed: unexpected invalid address'
       );
+    });
+
+    const prepareExpected = () => {
+      const address = 'some address';
+      const pin = '12345';
+      component.address = address;
+      return { address, pin };
+    };
+
+    it('opens wallet', async () => {
+      const expected = prepareExpected();
+      // Satisfy OpenWalletGuard for navigation
+      stubActiveSession(TestBed.inject(SessionStore));
+
+      await component.onPinConfirmed(expected.pin);
+
+      expect(openWalletSpy).toHaveBeenCalledOnceWith(
+        expected.address,
+        expected.pin
+      );
+      openWalletSpy.calls.reset();
+
+      expect(router.url).toBe('/wallet');
+    });
+
+    it('shows open wallet error', async () => {
+      const expected = prepareExpected();
+      const expectedError = 'open walled failed';
+      openWalletSpy.and.resolveTo(expectedError);
+
+      await component.onPinConfirmed(expected.pin);
+
+      expect(openWalletSpy).toHaveBeenCalledOnceWith(
+        expected.address,
+        expected.pin
+      );
+      openWalletSpy.calls.reset();
+
+      expect(swalSpy).toHaveBeenCalledOnceWith({
+        icon: 'error',
+        title: 'Open Wallet Failed',
+        text: expectedError,
+      });
+      swalSpy.calls.reset();
     });
   });
 });
