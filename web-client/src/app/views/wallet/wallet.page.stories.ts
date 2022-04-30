90cd503b07ef6a6d26911eeca82b880412222b71
Pi Delport <pi@ntls.io>
2022-04-30 02:14:12 +0200

    feat(web-client,WalletPage): configure actions based on KYC check status

--- web-client/src/app/views/wallet/wallet.page.stories.ts
+++ web-client/src/app/views/wallet/wallet.page.stories.ts
@@ -1,5 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Story } from '@storybook/angular';
+import { of } from 'rxjs';
+import { assetAmountAlgo } from 'src/app/utils/assets/assets.algo';
+import { AssetAmount } from 'src/app/utils/assets/assets.common';
+import { assetAmountXrp } from 'src/app/utils/assets/assets.xrp';
 import { WalletDisplay } from 'src/schema/entities';
 import {
   ionicStoryMeta,
@@ -8,23 +12,70 @@ import {
 import { WalletPageModule } from './wallet.module';
 import { WalletPage } from './wallet.page';
 
-export default ionicStoryMeta<WalletDisplay>(
+type Args = {
+  name?: string;
+  balances?: AssetAmount[];
+  requireKycBeforeSendPayment?: boolean;
+  onfidoCheckIsClear?: boolean;
+};
+
+export default ionicStoryMeta<Args>(
   {
     title: 'Views/WalletPage',
     component: WalletPage,
   },
   {
     imports: [HttpClientTestingModule, WalletPageModule],
-    controls: { shown: ['owner_name'] },
+    controls: {
+      shown: [
+        'name',
+        'balances',
+        'requireKycBeforeSendPayment',
+        'onfidoCheckIsClear',
+      ],
+    },
     layoutType: 'page',
   }
 );
 
-const Template: Story<WalletDisplay> = (wallet: WalletDisplay) => ({
+const Template: Story<Args> = ({
+  name,
+  balances,
+  onfidoCheckIsClear,
+  requireKycBeforeSendPayment,
+}) => ({
   moduleMetadata: {
-    providers: [provideSessionStore({ wallet })],
+    providers: [
+      provideSessionStore({
+        wallet: { owner_name: name } as WalletDisplay,
+        ...(onfidoCheckIsClear
+          ? { onfidoCheck: { id: 'id', href: '', result: 'clear' } }
+          : {}),
+      }),
+    ],
+  },
+  props: {
+    balances: of(balances),
+    requireKycBeforeSendPayment,
   },
 });
 
 export const Default = Template.bind({});
-Default.args = { owner_name: 'Test Owner' };
+Default.args = {
+  name: 'Test Owner',
+  balances: [assetAmountAlgo(1234.56), assetAmountXrp(123.456789)],
+};
+
+export const KYCNotCleared = Template.bind({});
+KYCNotCleared.args = {
+  ...Default.args,
+  requireKycBeforeSendPayment: true,
+  onfidoCheckIsClear: false,
+};
+
+export const KYCCleared = Template.bind({});
+KYCCleared.args = {
+  ...Default.args,
+  requireKycBeforeSendPayment: true,
+  onfidoCheckIsClear: true,
+};
