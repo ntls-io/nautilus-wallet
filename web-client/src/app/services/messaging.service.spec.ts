a6309051a0b555878d0635b68fb440943e99eb5e
Pi Delport <pi@ntls.io>
2022-05-04 01:47:46 +0200

    chore(web-client,MessagingService): ng generate
    Invocation:
    
    ```
    ng generate service services/messaging
    ```

---
+++ web-client/src/app/services/messaging.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { MessagingService } from './messaging.service';
+
+describe('MessagingService', () => {
+  let service: MessagingService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MessagingService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
