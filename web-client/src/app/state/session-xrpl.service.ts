4ed4ace8168a40808d8e8f15399e99dcf0939755
Pi Delport <pi@ntls.io>
2022-05-03 15:17:28 +0200

    feat(web-client,SessionXrplService): add trustline opt-in helpers

--- web-client/src/app/state/session-xrpl.service.ts
+++ web-client/src/app/state/session-xrpl.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { EnclaveService } from 'src/app/services/enclave/index';
 import { XrplService } from 'src/app/services/xrpl.service';
 import {
@@ -10,6 +11,8 @@ import {
 import { SessionService } from 'src/app/state/session.service';
 import { withLoggedExchange } from 'src/app/utils/console.helpers';
 import { panic } from 'src/app/utils/errors/panic';
+import { parseNumber } from 'src/app/utils/validators';
+import { ifDefined } from 'src/helpers/helpers';
 import { TransactionSigned, TransactionToSign } from 'src/schema/actions';
 import * as xrpl from 'xrpl';
 import { IssuedCurrencyAmount } from 'xrpl/dist/npm/models/common/index';
@@ -107,6 +110,61 @@ export class SessionXrplService {
     );
 
     return await this.sendTransaction(preparedTx);
+  }
+
+  /**
+   * Check trustline opt-in for each of this account's trust lines.
+   *
+   * @return The responses to `TrustSet` transactions sent out (empty if none sent)
+   * @see checkTrustlineOptIn
+   */
+  async checkTrustlineOptIns(): Promise<xrpl.TxResponse[]> {
+    // TODO(Pi): Check for necessary owner reserves before sending.
+    //           See: https://xrpl.org/reserves.html
+
+    const trustLines =
+      (await firstValueFrom(this.sessionQuery.xrplTrustlines)) ?? [];
+
+    const txResponses: xrpl.TxResponse[] = [];
+    for (const trustLine of trustLines) {
+      ifDefined(await this.checkTrustlineOptIn(trustLine), (txResponse) =>
+        txResponses.push(txResponse)
+      );
+    }
+    return txResponses;
+  }
+
+  /**
+   * Helper: Check trustline opt-in for the given trust-line.
+   *
+   * This sends a `TrustSet` transaction matching the peer's limit
+   * if the active session's wallet's limit is zero.
+   *
+   * @return the `TrustSet` response, or undefined
+   */
+  async checkTrustlineOptIn(
+    trustline: Trustline
+  ): Promise<xrpl.TxResponse | undefined> {
+    const limit_peer = parseNumber(trustline.limit_peer);
+    if (limit_peer === undefined) {
+      throw panic(
+        'SessionXrplService.checkTrustlineOptIn: bad limit_peer:',
+        trustline
+      );
+    }
+
+    if (trustline.limit === '0' && 0 < limit_peer) {
+      const limitAmount = {
+        currency: trustline.currency,
+        issuer: trustline.account,
+        value: trustline.limit_peer, // XXX: For now, just match the peer's limit.
+      };
+      return await withLoggedExchange(
+        'SessionXrplService.checkTrustlineOptIn: sending TrustSet',
+        async () => await this.sendTrustSetTx(limitAmount),
+        limitAmount
+      );
+    }
   }
 
   /**
