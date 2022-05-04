3b264dcc35d47b24587bd9eae249409f275b3589
Pi Delport <pi@ntls.io>
2022-05-04 02:07:37 +0200

    feat(web-client,MessagingService): add sendMessage

--- web-client/src/app/services/messaging.service.spec.ts
+++ web-client/src/app/services/messaging.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { MessagingService } from './messaging.service';
 
@@ -5,7 +6,9 @@ describe('MessagingService', () => {
   let service: MessagingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MessagingService);
   });
 
