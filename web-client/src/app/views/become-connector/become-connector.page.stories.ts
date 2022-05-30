dc85ee8495f457fdc3ee18e393309dc6864cd9e3
Jonathan Ngbonga <jonathan@ntls.io>
2022-05-30 15:58:18 +0200

    test(web-client): add story

---
+++ web-client/src/app/views/become-connector/become-connector.page.stories.ts
@@ -0,0 +1,21 @@
+import { Story } from '@storybook/angular';
+import { SharedModule } from 'src/app/modules/shared/shared.module';
+import { ionicStoryMeta } from 'src/stories/storybook.helpers';
+import { BecomeConnectorPage } from './become-connector.page';
+
+export default ionicStoryMeta<BecomeConnectorPage>(
+  {
+    title: 'Views/HomePage',
+    component: BecomeConnectorPage,
+  },
+  {
+    imports: [SharedModule],
+    layoutType: 'page',
+  }
+);
+
+const Template: Story<BecomeConnectorPage> = (args: BecomeConnectorPage) => ({
+  props: args,
+});
+
+export const Default = Template.bind({});
