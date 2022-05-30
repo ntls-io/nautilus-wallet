afcdc020ca1523062d70979070eb53da539bf788
Jonathan Ngbonga <jonathan@ntls.io>
2022-05-30 17:12:25 +0200

    test(web-client): add ReactiveFormsModule

--- web-client/src/app/views/become-connector/become-connector.module.ts
+++ web-client/src/app/views/become-connector/become-connector.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { BecomeConnectorPageRoutingModule } from './become-connector-routing.module';
@@ -9,6 +9,7 @@ import { BecomeConnectorPage } from './become-connector.page';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     BecomeConnectorPageRoutingModule,
