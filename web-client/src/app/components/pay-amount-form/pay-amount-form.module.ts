3530450d297c911190fccc78aeb2f0e3c353c57b
Jonathan Ngbonga <12111716+jongbonga@users.noreply.github.com>
2022-06-07 17:49:19 +0200

    feat(web-client): show transaction fees (#265)

--- web-client/src/app/components/pay-amount-form/pay-amount-form.module.ts
+++ web-client/src/app/components/pay-amount-form/pay-amount-form.module.ts
@@ -1,12 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AssetPipesModule } from 'src/app/pipes/asset-pipes.module';
+import { PureWalletPageComponentModule } from '../pure-wallet-page/pure-wallet-page.module';
+import { TransactionFeesComponentModule } from '../transaction-fees/transaction-fees.module';
 import { PayAmountFormComponent } from './pay-amount-form.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, IonicModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    IonicModule,
+    AssetPipesModule,
+    TransactionFeesComponentModule,
+    PureWalletPageComponentModule,
+  ],
   declarations: [PayAmountFormComponent],
   exports: [PayAmountFormComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PayAmountFormComponentModule {}
