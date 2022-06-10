0b8cda6974f45c94f5d2fd7390523c3c282f7b68
IscoRuta98 <fmruta98@gmail.com>
2022-06-10 13:40:38 +0200

    fix(web-client): Import missing testing modules

--- web-client/src/app/views/delete-user/delete-user.page.spec.ts
+++ web-client/src/app/views/delete-user/delete-user.page.spec.ts
@@ -1,22 +1,34 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { routes } from '../send-funds/send-funds-routing.module';
 import { DeleteUserPage } from './delete-user.page';
 
 describe('DeleteUserPage', () => {
+  let router: Router;
   let component: DeleteUserPage;
   let fixture: ComponentFixture<DeleteUserPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DeleteUserPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [DeleteUserPage],
+        imports: [
+          IonicModule.forRoot(),
+          RouterTestingModule.withRoutes(routes),
+          HttpClientTestingModule,
+        ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(DeleteUserPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      router = TestBed.inject(Router)
+      router.navigate(['delete-user'])
+      fixture = TestBed.createComponent(DeleteUserPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- web-client/src/app/views/deposit-funds/deposit-funds.page.spec.ts
+++ web-client/src/app/views/deposit-funds/deposit-funds.page.spec.ts
@@ -1,22 +1,34 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { routes } from '../../app-routing.module';
 import { DepositFundsPage } from './deposit-funds.page';
 
 describe('DepositFundsPage', () => {
+  let router: Router;
   let component: DepositFundsPage;
   let fixture: ComponentFixture<DepositFundsPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DepositFundsPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [DepositFundsPage],
+        imports: [
+          IonicModule.forRoot(),
+          RouterTestingModule.withRoutes(routes),
+          HttpClientTestingModule
+        ],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(DepositFundsPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      router = TestBed.inject(Router)
+      router.navigate(['deposit-funds'])
+      fixture = TestBed.createComponent(DepositFundsPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
