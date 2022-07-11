9d5e59108d43fa3cc1deef99300540843fccdfdb
Jonathan Ngbonga <jonathan@ntls.io>
2022-07-11 13:11:36 +0200

    fix(web-client): revert environment to local

--- web-client/src/environments/environment.ts
+++ web-client/src/environments/environment.ts
@@ -1,25 +1,41 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 import { Environment } from 'src/environments/types';
 
 export const environment: Environment = {
   production: false,
 
-  // Enable persistence for demo purposes.
+  // Enable persistence for easier development.
   persistAkitaState: true,
-  nautilusWalletServer: 'https://wallet-demo-api.ntls.io/',
-  nautilusAssetServices: 'https://wallet-demo-services.ntls.io/',
+
+  nautilusWalletServer: 'http://localhost:4200/api/nautilus/',
+  nautilusAssetServices: 'http://localhost:4200/api/asset-services/',
+  // See `proxyConfig` in `angular.json`, and `proxy.conf.json`
+  // Docs: https://angular.io/guide/build#proxying-to-a-backend-server
   algod: {
-    baseServer: 'https://testnet-algorand.api.purestake.io/ps2',
-    port: '',
+    // XXX: Algodv2's parameter handling is a bit weird: the HTTP port must be passed separately.
+    baseServer: 'http://localhost/api/algorand',
+    port: 4200,
     // FIXME: Development key
-    token: { 'X-API-Key': 'J7eo2jPb5m4OiBneIV6r0ajgRLeSaHqk3QplGETk' },
+    token: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
   },
-  // USDC from https://testnet.folks.finance/faucet
-  defaultAlgorandAssetId: 67395862,
+
   xrplClient: {
-    server: 'wss://s.altnet.rippletest.net/',
+    server: 'ws://localhost:4200/api/xrpl',
     options: {
       connectionTimeout: 20000,
     },
   },
   commissionPercentage: 0.01,
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
