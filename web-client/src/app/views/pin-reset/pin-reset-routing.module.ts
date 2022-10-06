07667ea33960fda05ddf52ce91189b13300b3925
Jonathan Ngbonga <jonathan@ntls.io>
2022-10-06 14:45:16 +0200

    test(web-client): run prettier

--- web-client/src/app/views/pin-reset/pin-reset-routing.module.ts
+++ web-client/src/app/views/pin-reset/pin-reset-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { PinResetPage } from './pin-reset.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: PinResetPage
-  }
+    component: PinResetPage,
+  },
 ];
 
 @NgModule({
--- web-client/src/app/views/pin-reset/pin-reset.page.spec.ts
+++ web-client/src/app/views/pin-reset/pin-reset.page.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { PinResetPage } from './pin-reset.page';
 
 describe('PinResetPage', () => {
   let component: PinResetPage;
   let fixture: ComponentFixture<PinResetPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ PinResetPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [PinResetPage],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(PinResetPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(PinResetPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
