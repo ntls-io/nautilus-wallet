0073b42101dcc5788911a0b9d628acbc477e5184
IscoRuta98 <fmruta98@gmail.com>
2022-10-17 08:10:49 +0200

    fix(web-client): Add HTTPTestingModule to DeleteUser service.spec file.

--- web-client/src/app/services/delete-user.service.spec.ts
+++ web-client/src/app/services/delete-user.service.spec.ts
@@ -1,12 +1,14 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { DeleteUserService } from './delete-user.service';
 
 describe('DeleteUserService', () => {
   let service: DeleteUserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DeleteUserService);
   });
 
