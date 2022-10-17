aaef347fe516a445a01d9ed6e577beed5afd5441
IscoRuta98 <fmruta98@gmail.com>
2022-10-17 09:05:10 +0200

    style(web-client): Run prettier

--- web-client/src/app/services/delete-user.service.spec.ts
+++ web-client/src/app/services/delete-user.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { DeleteUserService } from './delete-user.service';
 
 describe('DeleteUserService', () => {
