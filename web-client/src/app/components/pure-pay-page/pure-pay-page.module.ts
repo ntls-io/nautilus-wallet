6e0158306d3106253e1a720c26c2321b4c865793
Francisco Rutayebesibwa <68713894+IscoRuta98@users.noreply.github.com>
2022-11-03 11:20:49 +0200

    fix(web-client): Fix Blank payment page (#401)
    * fix(web-client): Fix Blank payment page
    
    * style(web-client): run prettier
    
    * fix(web-client): Hide scroll bar

--- web-client/src/app/components/pure-pay-page/pure-pay-page.module.ts
+++ web-client/src/app/components/pure-pay-page/pure-pay-page.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { PayComponentModule } from 'src/app/components/pay/pay.module';
+import { SharedModule } from '../../modules/shared/shared.module';
 import { PurePayPageComponent } from './pure-pay-page.component';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, PayComponentModule],
+  imports: [CommonModule, IonicModule, PayComponentModule, SharedModule],
   declarations: [PurePayPageComponent],
   exports: [PurePayPageComponent],
 })
