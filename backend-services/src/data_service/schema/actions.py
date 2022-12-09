ce18259cd65d6774089a1552613680cddf07c901
Jean-Pierre de Villiers <jean-pierre@ntls.io>
2022-12-09 06:41:22 +0200

    chore(backend): remove dead code

--- backend-services/src/data_service/schema/actions.py
+++ backend-services/src/data_service/schema/actions.py
@@ -1,20 +1,7 @@
-from enum import Enum
-from typing import TypeAlias
-
 from pydantic import BaseModel, Field
 
 from common.types import WalletAddress
 from data_service.schema.entities import Bookmark, BookmarkList
-
-
-class ActionMethod(str, Enum):
-    """
-    Names of the various available methods.
-    """
-
-    CREATE_BOOKMARK = "CreateBookmark"
-    DELETE_BOOKMARK = "DeleteBookmark"
-    GET_BOOKMARKS = "GetBookmarks"
 
 
 class CreateBookmark(BaseModel):
@@ -51,30 +38,9 @@ class DeleteBookmarkResult(BaseModel):
     success: bool
 
 
-class GetBookmarks(BaseModel):
-    """
-    Bookmark retrieval parameters.
-    """
-
-    wallet_id: WalletAddress
-
-
 class GetBookmarksResult(BaseModel):
     """
     Contains a list of bookmarks when successful or `None` otherwise.
     """
 
     bookmarks: BookmarkList | None = Field(...)
-
-
-Response: TypeAlias = CreateBookmarkResult | DeleteBookmarkResult | GetBookmarksResult
-RequestParams: TypeAlias = CreateBookmark | DeleteBookmark | GetBookmarks
-
-
-class Action(BaseModel):
-    """
-    Representation of a method call to the data service.
-    """
-
-    method: ActionMethod
-    params: RequestParams
