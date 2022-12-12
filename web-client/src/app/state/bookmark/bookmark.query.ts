aba99d37490b569ee93c62b0b5f4ea00451e8918
Jonathan Ngbonga <jonathan@ntls.io>
2022-12-12 21:48:05 +0200

    style(web-client): run prettier

--- web-client/src/app/state/bookmark/bookmark.query.ts
+++ web-client/src/app/state/bookmark/bookmark.query.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { QueryEntity } from '@datorama/akita';
-import { BookmarkStore, BookmarkState } from './bookmark.store';
+import { BookmarkState, BookmarkStore } from './bookmark.store';
 
 @Injectable({ providedIn: 'root' })
 export class BookmarkQuery extends QueryEntity<BookmarkState> {
-
   constructor(protected store: BookmarkStore) {
     super(store);
   }
-
 }
