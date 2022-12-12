358ac42f3c23dc1f2ad594ea9131418890aa515c
Jonathan Ngbonga <jonathan@ntls.io>
2022-12-12 13:06:27 +0200

    build(web-client): add assets generation hook

---
+++ web-client/scripts/generate-assets.js
@@ -0,0 +1,18 @@
+var exec = require("child_process").exec;
+
+module.exports = function (ctx) {
+  var configuration = ctx.build.configuration;
+  var organisation = configuration.split("-")[0];
+  var path = ["bhutan", "palau"].includes(organisation)
+    ? "--assetPath assets/" + organisation
+    : "";
+
+  var command = "npx capacitor-assets generate " + path;
+
+  console.log(">", command);
+
+  var logs = exec(command);
+  logs.stdout.on("data", function (data) {
+    console.log(data);
+  });
+};
