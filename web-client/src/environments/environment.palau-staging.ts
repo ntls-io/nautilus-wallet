118f7cf4c0d30d8e632d3c39feaacee12d3882de
Joseph Bochenek <bochenek.joe@gmail.com>
2022-12-14 17:49:55 +0200

    feat(web-client): Pull payment flag (#487)

--- web-client/src/environments/environment.palau-staging.ts
+++ web-client/src/environments/environment.palau-staging.ts
@@ -39,6 +39,7 @@ export const environment: Environment = {
   xrpIssuer: 'rJsc4o9Kh5xx6ba2pR62FbFpYJoh3xetMk',
   hideXrpBalance: true,
   autofundXrp: true,
+  hidePullPayment: false,
   autofundXrpAmount: 50,
   autofundAccountPin: '@CsmRxaR9UDXWfwy.rMv',
   autofundXrpPublicKey:
--- web-client/src/environments/environment.prod.ts
+++ web-client/src/environments/environment.prod.ts
@@ -39,6 +39,7 @@ export const environment: Environment = {
   xrpIssuer: 'rGqPqDW7PDNtRPyjFQ7fR47ojc3nUA5uBY',
   hideXrpBalance: false,
   autofundXrp: true,
+  hidePullPayment: true,
   autofundXrpAmount: 50,
   autofundAccountPin: 'K3VG-M@YQF9gzjq.DGW!',
   autofundXrpPublicKey:
