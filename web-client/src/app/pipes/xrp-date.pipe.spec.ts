c84f17c4688e5697fe3d4d1ba3d695d330402fef
Francisco Rutayebesibwa <fmruta98@gmail.com>
2023-04-24 14:51:46 +0200

    fix(web-client): Fix xrp-date test.

--- web-client/src/app/pipes/xrp-date.pipe.spec.ts
+++ web-client/src/app/pipes/xrp-date.pipe.spec.ts
@@ -1,8 +1,19 @@
+import { DatePipe } from '@angular/common';
+import { TestBed } from '@angular/core/testing';
 import { XrpDatePipe } from './xrp-date.pipe';
 
 describe('XrpDatePipe', () => {
+  let pipe: XrpDatePipe;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [DatePipe],
+    });
+
+    pipe = TestBed.inject(XrpDatePipe);
+  });
+
   it('create an instance', () => {
-    const pipe = new XrpDatePipe();
     expect(pipe).toBeTruthy();
   });
 });
