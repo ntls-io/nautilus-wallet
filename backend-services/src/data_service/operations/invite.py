b920e72071aa902bcb8fee8b2a041e07eb6aeaef
Jean-Pierre de Villiers <jean-pierre@ntls.io>
2023-04-25 10:29:38 +0200

    fix(backend-services): invalid query parameter

--- backend-services/src/data_service/operations/invite.py
+++ backend-services/src/data_service/operations/invite.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 from data_service.schema.actions import RedeemInvite
 from data_service.schema.entities import Invite
 from data_service.schema.types import Engine
+from odmantic import ObjectId
 
 
 async def invite(engine: Engine, invite_code: str) -> Invite:
@@ -10,7 +11,7 @@ async def invite(engine: Engine, invite_code: str) -> Invite:
     Retrieve an unredeemed invite from the database, if available.
     """
     existing_invite = await engine.find_one(
-        Invite, (Invite.code == invite_code) & (Invite.redeemed is False)
+        Invite, (Invite.code == invite_code) & (Invite.redeemed == False)  # noqa: E712
     )
     if existing_invite is None:
         raise HTTPException(404)
--- backend-services/tests/data_service/operations/test_invite.py
+++ backend-services/tests/data_service/operations/test_invite.py
@@ -31,7 +31,8 @@ async def test_get_invite_success(mocker: MockerFixture) -> None:
 
     assert await invite(engine, test_invite_code) == test_invite
     mock_find_one.assert_awaited_once_with(
-        Invite, (Invite.code == test_invite_code) & (Invite.redeemed is False)
+        Invite,
+        (Invite.code == test_invite_code) & (Invite.redeemed == False),  # noqa: E712
     )
 
 
