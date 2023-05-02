e5e6c818191500d69e9d116d620e18eec8b7b551
Francisco Rutayebesibwa <68713894+IscoRuta98@users.noreply.github.com>
2023-05-02 14:00:46 +0200

    fix(backend-service): get rid of conditional variable. (#556)
    * fix(backend-service): get rid of condtional variable.
    
    * fix(backend): remove condtion.
    
    * style: fix format with poetry black
    
    ---------
    
    Co-authored-by: Bill Guo <bill@ntls.io>

--- backend-services/src/data_service/operations/invite.py
+++ backend-services/src/data_service/operations/invite.py
@@ -10,9 +10,7 @@ async def invite(engine: Engine, invite_code: str) -> Invite:
     """
     Retrieve an unredeemed invite from the database, if available.
     """
-    existing_invite = await engine.find_one(
-        Invite, (Invite.code == invite_code) & (Invite.redeemed == False)  # noqa: E712
-    )
+    existing_invite = await engine.find_one(Invite, (Invite.code == invite_code))
     if existing_invite is None:
         raise HTTPException(404)
     if existing_invite.redeemed:
