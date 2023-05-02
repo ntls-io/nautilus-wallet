35f719a839a534e469ab71a5225325df6ed0d63b
Francisco Rutayebesibwa <68713894+IscoRuta98@users.noreply.github.com>
2023-05-02 11:20:38 +0200

    refactor(backend): Include case where invite code is redeemed. (#554)
    * refactor(backend): Include case where invite code is redeemed.
    
    * fix(backend): Use if statement for test (ruff) to pass.

--- backend-services/src/data_service/operations/invite.py
+++ backend-services/src/data_service/operations/invite.py
@@ -15,6 +15,8 @@ async def invite(engine: Engine, invite_code: str) -> Invite:
     )
     if existing_invite is None:
         raise HTTPException(404)
+    if existing_invite.redeemed:
+        raise HTTPException(422, detail="This invite code has already been redeemed.")
     return existing_invite
 
 
