edc02a5b720119ccacc688cd389a1c961ae20dcb
Bill Guo <bill@ntls.io>
2023-05-22 18:44:55 +0200

    fix(backend-services): set correct event loop for OTP Twilio async call

--- backend-services/src/auth_service/operations/twilio_2fa.py
+++ backend-services/src/auth_service/operations/twilio_2fa.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import HTTPException
 from twilio.rest import Client
 
@@ -13,9 +15,14 @@ from common.settings import AppSettings
 async def send_otp_to_user(
     settings: AppSettings, client: Client, params: SendOtp
 ) -> SendOtpResponse:
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)  # Set the Twilio client's event loop as the default
+
     verification = await client.verify.v2.services(
         settings.twilio_service_sid
     ).verifications.create_async(to=params.phone_number, channel="sms")
+
+    asyncio.set_event_loop(None)  # Reset the default event loop
 
     if verification.sid is None:
         raise HTTPException(500)
