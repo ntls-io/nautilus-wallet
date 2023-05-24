ec9ebb756a826d5e2f696fb0337b9fd94c15aa40
Bill Guo <bill@ntls.io>
2023-05-24 09:39:32 +0200

    fix(backend-services): allow put requests for CORS policy

--- backend-services/src/web_asgi/main.py
+++ backend-services/src/web_asgi/main.py
@@ -73,7 +73,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "HEAD", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "HEAD", "DELETE"],
     allow_headers=["*"],
 )
 
